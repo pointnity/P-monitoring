e255f3be657535b7f07bcf546d81dcedeb9ed2b9
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-08-03 13:39:23 +0800

    Update startup.bat

--- src/zoo/websocket/server/startup.bat
+++ src/zoo/websocket/server/startup.bat
@@ -5,3 +5,5 @@ setlocal & pushd
 set APP_ENTRY=org.tio.examples.im.server.ImServerStarter
 set BASE=%~dp0
 set CP=%BASE%\config;%BASE%\lib\*
+java -Xverify:none -XX:+HeapDumpOnOutOfMemoryError -Dtio.default.read.buffer.size=512 -XX:HeapDumpPath=c:/java-t-io-im-server-pid.hprof -cp "%CP%" %APP_ENTRY%
+endlocal & popd
