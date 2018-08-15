ce10dc90df862d0aadd9ca94dcd64e07e07ef03e
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-08-15 15:11:30 +0800

    Create WsServerConfig.java

---
+++ src/zoo/websocket/server/server/WsServerConfig.java
@@ -0,0 +1,21 @@
+package org.tio.websocket.server;
+
+import org.tio.http.common.HttpConst;
+
+/**
+ * @author tanyaowu
+ * June 28, 2017 2:42:59 PM
+ */
+public class WsServerConfig {
+
+	/**
+	 * @param args
+	 * @author tanyaowu
+	 */
+	public static void main(String[] args) {
+
+	}
+
+	private String bindIp = null;//"127.0.0.1";
+
+	private Integer bindPort = 9322;
