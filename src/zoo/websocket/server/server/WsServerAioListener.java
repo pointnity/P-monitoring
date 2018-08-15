bb41e506b2e1a19d913752fbc0cc3678160fbd9a
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-08-15 15:15:39 +0800

    Update WsServerAioListener.java

--- src/zoo/websocket/server/server/WsServerAioListener.java
+++ src/zoo/websocket/server/server/WsServerAioListener.java
@@ -6,3 +6,20 @@ import org.tio.server.intf.ServerAioListener;
 import org.tio.websocket.common.WsSessionContext;
 
 /**
+ *
+ * @author tanyaowu
+ * July 30, 2017 Morning9:16:02
+ */
+public class WsServerAioListener implements ServerAioListener {
+
+
+	public WsServerAioListener() {
+	}
+
+	@Override
+	public void onAfterClose(ChannelContext channelContext, Throwable throwable, String remark, boolean isRemove) {
+	}
+
+	@Override
+	public void onAfterConnected(ChannelContext channelContext, boolean isConnected, boolean isReconnect) {
+		WsSessionContext wsSessionContext = new WsSessionContext();
