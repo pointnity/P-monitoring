f019d0ea0860da17a33cf283c88b4e4fa0537e64
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-08-15 14:52:30 +0800

    Create   WsServerStarter.java

---
+++ src/zoo/websocket/server/server/WsServerStarter.java
@@ -0,0 +1,13 @@
+package org.tio.websocket.server;
+
+import java.io.IOException;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.tio.core.intf.TioUuid;
+import org.tio.server.AioServer;
+import org.tio.server.ServerGroupContext;
+import org.tio.utils.thread.pool.SynThreadPoolExecutor;
+import org.tio.websocket.common.WsTioUuid;
+import org.tio.websocket.server.handler.IWsMsgHandler;
