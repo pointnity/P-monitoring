fa49a652488825d49e1a56b21bc5b782c3010992
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-08-23 13:16:00 +0800

    Update and rename WsServerAioListener.javaWsServerAioListener.java to WsServerAioListener.java

---
+++ src/zoo/websocket/server/src/main/java/org/tio/websocket/server/WsServerAioListener.java
@@ -0,0 +1,12 @@
+package org.tio.websocket.server;
+
+import org.tio.core.ChannelContext;
+import org.tio.core.intf.Packet;
+import org.tio.server.intf.ServerAioListener;
+import org.tio.websocket.common.WsSessionContext;
+
+/**
+ *
+ * @author tanyaowu
+ * 
+ */
--- src/zoo/websocket/server/src/main/java/org/tio/websocket/server/WsServerAioListener.javaWsServerAioListener.java
+++
@@ -1,4 +0,0 @@
-package org.tio.websocket.server;
-
-import org.tio.core.ChannelContext;
-import org.tio.core.intf.Packet;
