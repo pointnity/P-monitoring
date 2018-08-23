3d5b97bc2c9bd3fb952497bace353a37db73b3f4
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-08-23 13:30:59 +0800

    Update IWsMsgHandler.java

--- src/zoo/websocket/server/src/main/java/org/tio/websocket/server/handler/IWsMsgHandler.java
+++ src/zoo/websocket/server/src/main/java/org/tio/websocket/server/handler/IWsMsgHandler.java
@@ -10,3 +10,7 @@ import org.tio.websocket.common.WsRequest;
  * @author tanyaowu
  * 
  */
+public interface IWsMsgHandler {
+	/**
+	 * The HttpResponse parameter is supplemented and returned, and if NULL indicates that the connection is not established, the framework disconnects, and if a non null is returned, the frame sends the object to the other
+   
