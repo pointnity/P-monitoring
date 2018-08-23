8da790a6c30a95bc7267ba4b03b26dfe1fa55d0f
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-08-23 13:29:07 +0800

    Update IWsMsgHandler.java

--- src/zoo/websocket/server/src/main/java/org/tio/websocket/server/handler/IWsMsgHandler.java
+++ src/zoo/websocket/server/src/main/java/org/tio/websocket/server/handler/IWsMsgHandler.java
@@ -3,3 +3,10 @@ package org.tio.websocket.server.handler;
 import org.tio.core.ChannelContext;
 import org.tio.http.common.HttpRequest;
 import org.tio.http.common.HttpResponse;
+import org.tio.websocket.common.WsRequest;
+
+/**
+ *
+ * @author tanyaowu
+ * 
+ */
