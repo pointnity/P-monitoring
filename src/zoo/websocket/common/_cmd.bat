fb134953b4c6d9a1e10febb0dec2e439d2c58231
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-08-24 14:02:11 +0800

    Update _cmd.bat

--- src/zoo/websocket/common/_cmd.bat
+++ src/zoo/websocket/common/_cmd.bat
@@ -3,3 +3,6 @@ echo -
 echo #Download source code
 echo mvn dependency:sources
 echo -
+
+echo #Download source code jar。 -DdownloadJavadocs=true Download Javadoc Package
+echo -DdownloadSources=true
