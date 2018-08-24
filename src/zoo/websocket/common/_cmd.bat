2c97c07f80dabc06739e5ad11978deb277444df5
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-08-24 14:04:18 +0800

    Update _cmd.bat

--- src/zoo/websocket/common/_cmd.bat
+++ src/zoo/websocket/common/_cmd.bat
@@ -22,3 +22,8 @@ echo -
 echo #Copy jar to warehouse directory ()
 echo mvn dependency:copy-dependencies -Dmdep.useRepositoryLayout=true -Dmdep.copyPom=true
 echo -
+echo -
+
+
+
+echo #Checking for version updates
