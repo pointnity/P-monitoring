0f09e122171d9c89ed6befddb8d4e3424d01e7a3
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-08-25 18:04:17 +0800

    Update WsPacket.java

--- src/zoo/websocket/common/src/main/java/org/tio/websocket/common/WsPacket.java
+++ src/zoo/websocket/common/src/main/java/org/tio/websocket/common/WsPacket.java
@@ -14,3 +14,7 @@ public class WsPacket extends Packet {
 	//	//Head with no cookies included
 	//	protected Map<String, String> headers = null;
 
+	private static final long serialVersionUID = 4506947563506841436L;
+	/**
+	 * The maximum number of message bodies
+	 */
