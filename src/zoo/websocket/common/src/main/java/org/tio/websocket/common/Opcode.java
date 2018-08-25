2612bf3fb24b42c344c10f9001729e702e70a88a
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-08-25 17:27:32 +0800

    Update Opcode.java

--- src/zoo/websocket/common/src/main/java/org/tio/websocket/common/Opcode.java
+++ src/zoo/websocket/common/src/main/java/org/tio/websocket/common/Opcode.java
@@ -7,3 +7,10 @@ import java.util.Map;
  * @author tanyaowu
  * 
  */
+public enum Opcode {
+
+	TEXT((byte) 1), BINARY((byte) 2), CLOSE((byte) 8), PING((byte) 9), PONG((byte) 10);
+
+	private static final Map<Byte, Opcode> map = new HashMap<>();
+
+	static {
