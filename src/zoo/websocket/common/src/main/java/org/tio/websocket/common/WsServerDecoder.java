75ff0d98877bc3fb34d12bd21f0db6103c800baa
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-08-26 19:14:28 +0800

    Update WsServerDecoder.java

--- src/zoo/websocket/common/src/main/java/org/tio/websocket/common/WsServerDecoder.java
+++ src/zoo/websocket/common/src/main/java/org/tio/websocket/common/WsServerDecoder.java
@@ -81,3 +81,26 @@ public class WsServerDecoder {
 			payloadLength = ByteBufferUtils.readUB2WithBigEdian(buf);
 			log.info("{} payloadLengthFlag: 126，payloadLength {}", channelContext, payloadLength);
 
+		} else if (payloadLength == 127) { //127 reads 8 bytes, and the latter 8 bytes is payloadlength
+			headLength += 8;
+			if (readableLength < headLength) {
+				return null;
+			}
+
+			payloadLength = (int) buf.getLong();
+			log.info("{} payloadLengthFlag: 127，payloadLength {}", channelContext, payloadLength);
+		}
+
+		if (payloadLength < 0 || payloadLength > WsPacket.MAX_BODY_LENGTH) {
+			throw new AioDecodeException("body length(" + payloadLength + ") is not right");
+		}
+
+		if (readableLength < headLength + payloadLength) {
+			return null;
+		}
+
+		if (hasMask) {
+			mask = ByteBufferUtils.readBytes(buf, 4);
+		}
+
+		//Second phase analysis
