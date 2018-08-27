27c93da6ee985ed218376379b03f84f9678b03fa
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-08-27 17:42:46 +0800

    Update WsSessionContext.java

--- src/zoo/websocket/common/src/main/java/org/tio/websocket/common/WsSessionContext.java
+++ src/zoo/websocket/common/src/main/java/org/tio/websocket/common/WsSessionContext.java
@@ -4,3 +4,11 @@ import java.util.List;
 
 import org.tio.http.common.HttpRequest;
 import org.tio.http.common.HttpResponse;
+
+/**
+ *
+ * @author tanyaowu
+ *
+ */
+public class WsSessionContext {
+	/**
