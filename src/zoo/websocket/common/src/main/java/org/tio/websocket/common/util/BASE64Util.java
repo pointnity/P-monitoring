8db835b7ffa21041abbc4df2402d7a2f98df9c73
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-08-28 15:47:46 +0800

    Update BASE64Util.java

--- src/zoo/websocket/common/src/main/java/org/tio/websocket/common/util/BASE64Util.java
+++ src/zoo/websocket/common/src/main/java/org/tio/websocket/common/util/BASE64Util.java
@@ -30,3 +30,4 @@ public class BASE64Util {
 	 * This array is a lookup table that translates 6-bit positive integer
 	 * index values into their "Alternate Base64 Alphabet" equivalents. This is
 	 * NOT the real Base64 Alphabet as per in Table 1 of RFC 2045. This
+	 * alternate alphabet does not use the capital letters. It is designed for
