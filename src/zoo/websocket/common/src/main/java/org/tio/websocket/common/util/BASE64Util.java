04376edc4ddd7e4d03d79f4571cea4f22e751e81
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-08-28 15:47:59 +0800

    Update BASE64Util.java

--- src/zoo/websocket/common/src/main/java/org/tio/websocket/common/util/BASE64Util.java
+++ src/zoo/websocket/common/src/main/java/org/tio/websocket/common/util/BASE64Util.java
@@ -31,3 +31,4 @@ public class BASE64Util {
 	 * index values into their "Alternate Base64 Alphabet" equivalents. This is
 	 * NOT the real Base64 Alphabet as per in Table 1 of RFC 2045. This
 	 * alternate alphabet does not use the capital letters. It is designed for
+	 * use in environments where "case folding" occurs.
