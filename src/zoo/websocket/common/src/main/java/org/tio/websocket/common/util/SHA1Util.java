b541719c32778d9f3abcbe15fb3514e4560654ba
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-08-30 16:12:29 +0800

    Update SHA1Util.java

--- src/zoo/websocket/common/src/main/java/org/tio/websocket/common/util/SHA1Util.java
+++ src/zoo/websocket/common/src/main/java/org/tio/websocket/common/util/SHA1Util.java
@@ -2,5 +2,7 @@
  * Copyright  GenerallyCloud.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); *
- * you may not use this file except in compliance with the License.
+ * You may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
+ *
+ *    apache.org/licenses/LICENSE-2.0
