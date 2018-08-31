aae137f427aceeec02b42c43cd4be21917e31ce0
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-08-31 19:04:42 +0800

    Update _cmd.bat

--- src/zoo/websocket/demo/websocketserver-1/_cmd.bat
+++ src/zoo/websocket/demo/websocketserver-1/_cmd.bat
@@ -17,3 +17,5 @@ echo -
 echo #Copy jar to a directory (all jars in the same directory)
 echo mvn dependency:copy-dependencies -Dmdep.useRepositoryLayout=false
 echo -
+
+echo #Copy jar to warehouse directory ()
