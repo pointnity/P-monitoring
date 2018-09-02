8895ca403895ff1a0a464032ad3b1a58f66610d2
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-02 14:24:10 +0800

    Update startup.bat

--- src/zoo/websocket/demo/websocketserver-1/startup.bat
+++ src/zoo/websocket/demo/websocketserver-1/startup.bat
@@ -6,3 +6,4 @@ set APP_ENTRY=org.tio.websocket.server.demo1.WsDemoStarter
 set BASE=%~dp0
 set CP=%BASE%\config;%BASE%\lib\*
 java -Xverify:none -XX:+HeapDumpOnOutOfMemoryError -Dtio.default.read.buffer.size=512 -XX:HeapDumpPath=c:/java-t-io-wsdemo-server-pid.hprof -cp "%CP%" %APP_ENTRY%
+endlocal & popd
