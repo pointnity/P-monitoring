889a4bf3f48998aa5c4c5e357d99173b888c3007
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-03 15:16:23 +0800

    Update bytebuffer-min.js

--- src/zoo/websocket/demo/websocketserver-1/src/main/html/bytebuffer-min.js
+++ src/zoo/websocket/demo/websocketserver-1/src/main/html/bytebuffer-min.js
@@ -5,3 +5,5 @@ return factory(Long);})();else
 (global["dcodeIO"]=global["dcodeIO"]||{})["ByteBuffer"]=factory(global["dcodeIO"]["Long"]);})(this,function(Long){"use strict";var ByteBuffer=function(capacity,littleEndian,noAssert){if(typeof capacity==='undefined')
 capacity=ByteBuffer.DEFAULT_CAPACITY;if(typeof littleEndian==='undefined')
 littleEndian=ByteBuffer.DEFAULT_ENDIAN;if(typeof noAssert==='undefined')
+noAssert=ByteBuffer.DEFAULT_NOASSERT;if(!noAssert){capacity=capacity|0;if(capacity<0)
+throw RangeError("Illegal capacity");littleEndian=!!littleEndian;noAssert=!!noAssert;}
