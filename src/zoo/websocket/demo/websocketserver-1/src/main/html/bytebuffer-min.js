752d829ee1ea8aec2040416917fdcf1eebfeb632
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-03 15:52:53 +0800

    Update bytebuffer-min.js

--- src/zoo/websocket/demo/websocketserver-1/src/main/html/bytebuffer-min.js
+++ src/zoo/websocket/demo/websocketserver-1/src/main/html/bytebuffer-min.js
@@ -385,3 +385,10 @@ dst(((cp>>12)&0x0F)|0xE0),dst(((cp>>6)&0x3F)|0x80),dst((cp&0x3F)|0x80);else
 dst(((cp>>18)&0x07)|0xF0),dst(((cp>>12)&0x3F)|0x80),dst(((cp>>6)&0x3F)|0x80),dst((cp&0x3F)|0x80);cp=null;}};utfx.decodeUTF8=function(src,dst){var a,b,c,d,fail=function(b){b=b.slice(0,b.indexOf(null));var err=Error(b.toString());err.name="TruncatedError";err['bytes']=b;throw err;};while((a=src())!==null){if((a&0x80)===0)
 dst(a);else if((a&0xE0)===0xC0)
 ((b=src())===null)&&fail([a,b]),dst(((a&0x1F)<<6)|(b&0x3F));else if((a&0xF0)===0xE0)
+((b=src())===null||(c=src())===null)&&fail([a,b,c]),dst(((a&0x0F)<<12)|((b&0x3F)<<6)|(c&0x3F));else if((a&0xF8)===0xF0)
+((b=src())===null||(c=src())===null||(d=src())===null)&&fail([a,b,c,d]),dst(((a&0x07)<<18)|((b&0x3F)<<12)|((c&0x3F)<<6)|(d&0x3F));else throw RangeError("Illegal starting byte: "+a);}};utfx.UTF16toUTF8=function(src,dst){var c1,c2=null;while(true){if((c1=c2!==null?c2:src())===null)
+break;if(c1>=0xD800&&c1<=0xDFFF){if((c2=src())!==null){if(c2>=0xDC00&&c2<=0xDFFF){dst((c1-0xD800)*0x400+c2-0xDC00+0x10000);c2=null;continue;}}}
+dst(c1);}
+if(c2!==null)dst(c2);};utfx.UTF8toUTF16=function(src,dst){var cp=null;if(typeof src==='number')
+cp=src,src=function(){return null;};while(cp!==null||(cp=src())!==null){if(cp<=0xFFFF)
+dst(cp);else
