872635d7e284680e2991c1168f119a3a2e084982
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-03 15:16:11 +0800

    Update bytebuffer-min.js

--- src/zoo/websocket/demo/websocketserver-1/src/main/html/bytebuffer-min.js
+++ src/zoo/websocket/demo/websocketserver-1/src/main/html/bytebuffer-min.js
@@ -2,3 +2,6 @@
 define(["long"],factory);else if(typeof require==='function'&&typeof module==="object"&&module&&module["exports"])
 module['exports']=(function(){var Long;try{Long=require("long");}catch(e){}
 return factory(Long);})();else
+(global["dcodeIO"]=global["dcodeIO"]||{})["ByteBuffer"]=factory(global["dcodeIO"]["Long"]);})(this,function(Long){"use strict";var ByteBuffer=function(capacity,littleEndian,noAssert){if(typeof capacity==='undefined')
+capacity=ByteBuffer.DEFAULT_CAPACITY;if(typeof littleEndian==='undefined')
+littleEndian=ByteBuffer.DEFAULT_ENDIAN;if(typeof noAssert==='undefined')
