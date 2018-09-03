917cc7882a9f6dff58ad8918f502f4f6ea3e8a9f
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-03 15:36:17 +0800

    Update bytebuffer-min.js

--- src/zoo/websocket/demo/websocketserver-1/src/main/html/bytebuffer-min.js
+++ src/zoo/websocket/demo/websocketserver-1/src/main/html/bytebuffer-min.js
@@ -130,3 +130,7 @@ return(s?-1:1)*m*Math.pow(2,e-mLen);}
 function ieee754_write(buffer,value,offset,isLE,mLen,nBytes){var e,m,c,eLen=nBytes*8-mLen-1,eMax=(1<<eLen)-1,eBias=eMax>>1,rt=(mLen===23?Math.pow(2,-24)-Math.pow(2,-77):0),i=isLE?0:(nBytes-1),d=isLE?1:-1,s=value<0||(value===0&&1/value<0)?1:0;value=Math.abs(value);if(isNaN(value)||value===Infinity){m=isNaN(value)?1:0;e=eMax;}else{e=Math.floor(Math.log(value)/Math.LN2);if(value*(c=Math.pow(2,-e))<1){e--;c*=2;}
 if(e+eBias>=1){value+=rt/c;}else{value+=rt*Math.pow(2,1-eBias);}
 if(value*c>=2){e++;c/=2;}
+if(e+eBias>=eMax){m=0;e=eMax;}else if(e+eBias>=1){m=(value*c-1)*Math.pow(2,mLen);e=e+eBias;}else{m=value*Math.pow(2,eBias-1)*Math.pow(2,mLen);e=0;}}
+for(;mLen>=8;buffer[offset+i]=m&0xff,i+=d,m/=256,mLen-=8){}
+e=(e<<mLen)|m;eLen+=mLen;for(;eLen>0;buffer[offset+i]=e&0xff,i+=d,e/=256,eLen-=8){}
+buffer[offset+i-d]|=s*128;}
