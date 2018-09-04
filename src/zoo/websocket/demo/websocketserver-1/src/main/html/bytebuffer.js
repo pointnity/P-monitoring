79410e65e7a8dbcc052764ff0a36816caa571490
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-04 17:11:28 +0800

    Update bytebuffer.js

--- src/zoo/websocket/demo/websocketserver-1/src/main/html/bytebuffer.js
+++ src/zoo/websocket/demo/websocketserver-1/src/main/html/bytebuffer.js
@@ -1,2 +1,8 @@
 /*
  Copyright Daniel Wirtz <dcode@dcode.io>
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ 
