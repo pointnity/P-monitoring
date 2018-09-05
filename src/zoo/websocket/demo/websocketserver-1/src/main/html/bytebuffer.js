c820cbe513ce00fa330f6d7ff81385ef1b772c19
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-05 13:53:33 +0800

    Update bytebuffer.js

--- src/zoo/websocket/demo/websocketserver-1/src/main/html/bytebuffer.js
+++ src/zoo/websocket/demo/websocketserver-1/src/main/html/bytebuffer.js
@@ -43,3 +43,4 @@
      * @param {number=} capacity Initial capacity. Defaults to {@link ByteBuffer.DEFAULT_CAPACITY}.
      * @param {boolean=} littleEndian Whether to use little or big endian byte order. Defaults to
      *  {@link ByteBuffer.DEFAULT_ENDIAN}.
+     * @param {boolean=} noAssert Whether to skip assertions of offsets and values. Defaults to
