c9edb905d753b44e6db5f0ceeae054d933a4c53e
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-06 12:02:54 +0800

    Update demoByte-2.js

--- src/zoo/websocket/demo/websocketserver-1/src/main/html/demoByte-2.js
+++ src/zoo/websocket/demo/websocketserver-1/src/main/html/demoByte-2.js
@@ -3,3 +3,6 @@ var ByteBuffer = window.dcodeIO.ByteBuffer;
 var ws;
 function initWs() {
     ws = new WebSocket("ws://localhost:9321?name=Kobe Bryant &name= Kobe Bryant Name= Kobe Bryant");
+    ws.binaryType = 'arraybuffer';
+    ws.onmessage = function (event) {
+        if(event.data instanceof ArrayBuffer){
