75f638a3f8651a16e12e3161b531d9997c677657
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-07 11:07:41 +0800

    Update demoText.js

--- src/zoo/websocket/demo/websocketserver-1/src/main/html/demoText.js
+++ src/zoo/websocket/demo/websocketserver-1/src/main/html/demoText.js
@@ -1,3 +1,6 @@
 var ws;
 function initWs() {
     ws = new WebSocket("ws://localhost:9321?name=Kobe Bryant &name= Kobe &name= in the library");
+    ws.onmessage = function (event) {
+        document.getElementById("contentId").value += (event.data + "\r\n");
+    };
