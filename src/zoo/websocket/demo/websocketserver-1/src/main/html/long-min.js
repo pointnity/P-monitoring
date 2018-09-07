c5effb82d850fef3be5432a2cf600501b8c303f2
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-07 11:21:19 +0800

    Update long-min.js

--- src/zoo/websocket/demo/websocketserver-1/src/main/html/long-min.js
+++ src/zoo/websocket/demo/websocketserver-1/src/main/html/long-min.js
@@ -24,3 +24,6 @@ throw Error('empty string');if(str==="NaN"||str==="Infinity"||str==="+Infinity"|
 return ZERO;if(typeof unsigned==='number'){radix=unsigned,unsigned=false;}else{unsigned=!!unsigned;}
 radix=radix||10;if(radix<2||36<radix)
 throw RangeError('radix');var p;if((p=str.indexOf('-'))>0)
+throw Error('interior hyphen');else if(p===0){return fromString(str.substring(1),unsigned,radix).neg();}
+var radixToPower=fromNumber(pow_dbl(radix,8));var result=ZERO;for(var i=0;i<str.length;i+=8){var size=Math.min(8,str.length-i),value=parseInt(str.substring(i,i+size),radix);if(size<8){var power=fromNumber(pow_dbl(radix,size));result=result.mul(power).add(fromNumber(value));}else{result=result.mul(radixToPower);result=result.add(fromNumber(value));}}
+result.unsigned=unsigned;return result;}
