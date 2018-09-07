66a13d047945414489a7094420db4a71a0d99a74
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-07 11:14:41 +0800

    Update long-min.js

--- src/zoo/websocket/demo/websocketserver-1/src/main/html/long-min.js
+++ src/zoo/websocket/demo/websocketserver-1/src/main/html/long-min.js
@@ -6,3 +6,5 @@ Long.prototype.__isLong__;Object.defineProperty(Long.prototype,"__isLong__",{val
 Long.isLong=isLong;var INT_CACHE={};var UINT_CACHE={};function fromInt(value,unsigned){var obj,cachedObj,cache;if(unsigned){value>>>=0;if(cache=(0<=value&&value<256)){cachedObj=UINT_CACHE[value];if(cachedObj)
 return cachedObj;}
 obj=fromBits(value,(value|0)<0?-1:0,true);if(cache)
+UINT_CACHE[value]=obj;return obj;}else{value|=0;if(cache=(-128<=value&&value<128)){cachedObj=INT_CACHE[value];if(cachedObj)
+return cachedObj;}
