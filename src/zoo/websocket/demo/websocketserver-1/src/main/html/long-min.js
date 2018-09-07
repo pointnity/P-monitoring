7e54ef3f850e7d73634add7318ecbf01670497e2
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-07 11:14:01 +0800

    Update long-min.js

--- src/zoo/websocket/demo/websocketserver-1/src/main/html/long-min.js
+++ src/zoo/websocket/demo/websocketserver-1/src/main/html/long-min.js
@@ -1,3 +1,7 @@
 (function(global,factory){if(typeof define==='function'&&define["amd"])
 define([],factory);else if(typeof require==='function'&&typeof module==="object"&&module&&module["exports"])
 module["exports"]=factory();else
+(global["dcodeIO"]=global["dcodeIO"]||{})["Long"]=factory();})(this,function(){"use strict";function Long(low,high,unsigned){this.low=low|0;this.high=high|0;this.unsigned=!!unsigned;}
+Long.prototype.__isLong__;Object.defineProperty(Long.prototype,"__isLong__",{value:true,enumerable:false,configurable:false});function isLong(obj){return(obj&&obj["__isLong__"])===true;}
+Long.isLong=isLong;var INT_CACHE={};var UINT_CACHE={};function fromInt(value,unsigned){var obj,cachedObj,cache;if(unsigned){value>>>=0;if(cache=(0<=value&&value<256)){cachedObj=UINT_CACHE[value];if(cachedObj)
+return cachedObj;}
