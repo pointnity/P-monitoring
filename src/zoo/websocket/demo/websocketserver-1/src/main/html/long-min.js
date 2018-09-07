70b8d98eb11b401d4b07183cc2116f4097348185
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-07 11:13:37 +0800

    Create long-min.js

---
+++ src/zoo/websocket/demo/websocketserver-1/src/main/html/long-min.js
@@ -0,0 +1,3 @@
+(function(global,factory){if(typeof define==='function'&&define["amd"])
+define([],factory);else if(typeof require==='function'&&typeof module==="object"&&module&&module["exports"])
+module["exports"]=factory();else
