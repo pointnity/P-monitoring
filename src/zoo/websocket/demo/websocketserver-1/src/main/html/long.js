afdba2904267da85ef4e89a9d6838d5e517c53d7
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-08 20:10:36 +0800

    Update long.js

--- src/zoo/websocket/demo/websocketserver-1/src/main/html/long.js
+++ src/zoo/websocket/demo/websocketserver-1/src/main/html/long.js
@@ -75,3 +75,4 @@ apache.org/licenses/LICENSE-2.0
     // Note that we must ALWAYS check specially whether those values are MIN_VALUE
     // (-2^63) because -MIN_VALUE == MIN_VALUE (since 2^63 cannot be represented as
     // a positive number, it overflows back into a negative).  Not handling this
+    // case would often result in infinite recursion.
