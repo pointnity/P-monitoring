7f4a01be671489a34453fda46c4bd0709e56b830
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-08 20:04:22 +0800

    Update long.js

--- src/zoo/websocket/demo/websocketserver-1/src/main/html/long.js
+++ src/zoo/websocket/demo/websocketserver-1/src/main/html/long.js
@@ -13,3 +13,7 @@ apache.org/licenses/LICENSE-2.0
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  See the License for the specific language governing permissions and
  limitations under the License.
+*/
+
+/**
+ * @license long.js (c) 2013 Daniel Wirtz <dcode@dcode.io>
