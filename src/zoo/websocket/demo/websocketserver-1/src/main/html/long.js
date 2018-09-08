a31be80993ee81e915d2bc3140aa37db3def05f3
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-08 20:16:44 +0800

    Update long.js

--- src/zoo/websocket/demo/websocketserver-1/src/main/html/long.js
+++ src/zoo/websocket/demo/websocketserver-1/src/main/html/long.js
@@ -131,3 +131,31 @@ apache.org/licenses/LICENSE-2.0
      * @returns {!Long}
      * @inner
      */
+    function fromInt(value, unsigned) {
+        var obj, cachedObj, cache;
+        if (unsigned) {
+            value >>>= 0;
+            if (cache = (0 <= value && value < 256)) {
+                cachedObj = UINT_CACHE[value];
+                if (cachedObj)
+                    return cachedObj;
+            }
+            obj = fromBits(value, (value | 0) < 0 ? -1 : 0, true);
+            if (cache)
+                UINT_CACHE[value] = obj;
+            return obj;
+        } else {
+            value |= 0;
+            if (cache = (-128 <= value && value < 128)) {
+                cachedObj = INT_CACHE[value];
+                if (cachedObj)
+                    return cachedObj;
+            }
+            obj = fromBits(value, value < 0 ? -1 : 0, false);
+            if (cache)
+                INT_CACHE[value] = obj;
+            return obj;
+        }
+    }
+
+    /**
