f428ff7b38708c7ec35166f5212572d3d59d93d2
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-08 20:03:34 +0800

    Update long.js

--- src/zoo/websocket/demo/websocketserver-1/src/main/html/long.js
+++ src/zoo/websocket/demo/websocketserver-1/src/main/html/long.js
@@ -1,3 +1,6 @@
 /*
  Copyright  Daniel Wirtz <dcode@dcode.io>
  Copyright  The Closure Library Authors. All Rights Reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
