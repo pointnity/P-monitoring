ddb484e752b0358da7edbf7f2e92ff1e2907d376
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-11 20:38:36 +0800

    Update _cmd.bat

--- src/zoo/websocket/parent/_cmd.bat
+++ src/zoo/websocket/parent/_cmd.bat
@@ -16,3 +16,4 @@ echo mvn dependency:unpack-dependencies
 echo -
 
 echo #Copy jar to a directory (all jars in the same directory)
+echo mvn dependency:copy-dependencies -Dmdep.useRepositoryLayout=false
