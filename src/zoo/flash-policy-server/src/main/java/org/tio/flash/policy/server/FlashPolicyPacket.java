5515dd2ca5f29f597780cc7e19ab3f608c5e9cc0
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-14 19:10:38 +0800

    Update FlashPolicyPacket.java

--- src/zoo/flash-policy-server/src/main/java/org/tio/flash/policy/server/FlashPolicyPacket.java
+++ src/zoo/flash-policy-server/src/main/java/org/tio/flash/policy/server/FlashPolicyPacket.java
@@ -10,3 +10,6 @@ import org.tio.core.intf.Packet;
 public class FlashPolicyPacket extends Packet {
 	private static final long serialVersionUID = -172060606924066412L;
 	public static final int MIN_LENGHT = 22;//The minimum length of the message
+	public static final int MAX_LING_LENGHT = 256;//Maximum length of a row
+	
+	public static final FlashPolicyPacket REQUEST = new FlashPolicyPacket();
