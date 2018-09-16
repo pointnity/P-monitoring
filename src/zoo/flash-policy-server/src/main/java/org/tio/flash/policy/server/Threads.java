c4a70b7c2e5dea86fba270f7a5451cd2362145e5
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-16 15:09:44 +0800

    Update Threads.java

--- src/zoo/flash-policy-server/src/main/java/org/tio/flash/policy/server/Threads.java
+++ src/zoo/flash-policy-server/src/main/java/org/tio/flash/policy/server/Threads.java
@@ -19,3 +19,5 @@ import org.tio.utils.thread.pool.SynThreadPoolExecutor;
 	public static int CORE_POOL_SIZE = Runtime.getRuntime().availableProcessors() * 1;
 
 	// public static final int CORE_POOL_SIZE = _CORE_POOL_SIZE;// < 160 ? 160 :
+	// _CORE_POOL_SIZE;
+
