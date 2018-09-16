b9dda50cfd215f7c8d51c746bf46f848de140b5f
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-16 15:12:57 +0800

    Update Threads.java

--- src/zoo/flash-policy-server/src/main/java/org/tio/flash/policy/server/Threads.java
+++ src/zoo/flash-policy-server/src/main/java/org/tio/flash/policy/server/Threads.java
@@ -32,3 +32,4 @@ import org.tio.utils.thread.pool.SynThreadPoolExecutor;
 		String tioThreadName = "tio";
 		tioExecutor = new SynThreadPoolExecutor(MAX_POOL_SIZE, MAX_POOL_SIZE, KEEP_ALIVE_TIME, tioQueue, DefaultThreadFactory.getInstance(tioThreadName, Thread.NORM_PRIORITY),
 				tioThreadName);
+		tioExecutor.prestartAllCoreThreads();
