3e78d02d1ba43503cb6a6aab95d7ec030bc8b5db
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-16 15:07:47 +0800

    Update Threads.java

--- src/zoo/flash-policy-server/src/main/java/org/tio/flash/policy/server/Threads.java
+++ src/zoo/flash-policy-server/src/main/java/org/tio/flash/policy/server/Threads.java
@@ -6,3 +6,5 @@ package org.tio.flash.policy.server;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+
+import org.tio.utils.thread.pool.DefaultThreadFactory;
