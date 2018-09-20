6831314e1d486889bbb90674d47d8f831e163d71
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-20 09:41:01 +0800

    Update HttpConfig.java

--- src/zoo/http/common/src/main/java/org/tio/http/common/HttpConfig.java
+++ src/zoo/http/common/src/main/java/org/tio/http/common/HttpConfig.java
@@ -4,3 +4,5 @@ import java.io.File;
 import java.io.IOException;
 
 import org.apache.commons.lang3.StringUtils;
+import org.tio.http.common.handler.HttpRequestHandler;
+import org.tio.http.common.session.id.ISessionIdGenerator;
