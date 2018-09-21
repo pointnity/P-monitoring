15019305a5baab128d2ea9329ceec5c807b6dbe1
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-21 09:37:03 +0800

    Update HttpConst.java

--- src/zoo/http/common/src/main/java/org/tio/http/common/HttpConst.java
+++ src/zoo/http/common/src/main/java/org/tio/http/common/HttpConst.java
@@ -106,3 +106,4 @@ public interface HttpConst {
 		/**
 		 * The encoding (Encode) method of the document.Can only be obtained after decoding Content-Type the content type specified by the header.
 		 * Using gzip to compress documents can significantly reduce the download time of HTML documents.
+		 * Java's gzipoutputstream can be easily gzip compressed, but only on Unix Netscape and IE 4, ie 5 on Windows.
