cce6d962726a7b29b4565f67d643be41f67cd6cd
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-21 09:24:48 +0800

    Update HttpConst.java

--- src/zoo/http/common/src/main/java/org/tio/http/common/HttpConst.java
+++ src/zoo/http/common/src/main/java/org/tio/http/common/HttpConst.java
@@ -23,3 +23,5 @@ public interface HttpConst {
 	                   Upgrade : websocket
 	                      Host : t-io.org:9321
 	           Accept-Encoding : gzip, deflate, sdch
+	                User-Agent : Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36
+	                    Origin 
