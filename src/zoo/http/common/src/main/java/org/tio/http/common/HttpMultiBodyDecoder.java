9dc2f2f95a636c8422cbe44537d73ecf20d42cd6
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-22 15:48:44 +0800

    Update HttpMultiBodyDecoder.java

--- src/zoo/http/common/src/main/java/org/tio/http/common/HttpMultiBodyDecoder.java
+++ src/zoo/http/common/src/main/java/org/tio/http/common/HttpMultiBodyDecoder.java
@@ -307,3 +307,12 @@ public class HttpMultiBodyDecoder {
 			header.setName(name);
 			header.setFilename(filename);
 			header.setContentType(contentType);
+
+		} catch (Throwable e) {
+			log.error(channelContext.toString(), e);
+			throw new AioDecodeException(e.toString());
+		}
+
+		//		for (int i = 0; i < lines.size(); i++) {
+		//			String line = lines.get(i);
+		//			if (i == 0) {
