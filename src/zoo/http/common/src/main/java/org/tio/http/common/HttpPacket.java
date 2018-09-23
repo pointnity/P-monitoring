8d24f69650057f9d0aa9a3fe36a6f79ba00121e7
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-23 15:26:43 +0800

    Update HttpPacket.java

--- src/zoo/http/common/src/main/java/org/tio/http/common/HttpPacket.java
+++ src/zoo/http/common/src/main/java/org/tio/http/common/HttpPacket.java
@@ -14,3 +14,6 @@ import org.tio.core.intf.Packet;
  */
 public class HttpPacket extends Packet {
 
+	//	private static Logger log = LoggerFactory.getLogger(HttpPacket.class);
+
+	private static final long serialVersionUID = 3903186670675671956L;
