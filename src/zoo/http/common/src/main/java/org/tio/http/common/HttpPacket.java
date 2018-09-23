fd82378eaff972581dfb3717e8b2620f90282948
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-23 15:25:57 +0800

    Update HttpPacket.java

--- src/zoo/http/common/src/main/java/org/tio/http/common/HttpPacket.java
+++ src/zoo/http/common/src/main/java/org/tio/http/common/HttpPacket.java
@@ -3,3 +3,6 @@ package org.tio.http.common;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.tio.core.intf.Packet;
