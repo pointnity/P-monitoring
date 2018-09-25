1e0a0ff1fc9fba0cf9d34fe04a99ff2ebcfd02ce
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-25 15:01:25 +0800

    Update HttpResponse.java

--- src/zoo/http/common/src/main/java/org/tio/http/common/HttpResponse.java
+++ src/zoo/http/common/src/main/java/org/tio/http/common/HttpResponse.java
@@ -5,3 +5,14 @@ import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author tanyaowu
+ *
+ */
+public class HttpResponse extends HttpPacket {
+	private static Logger log = LoggerFactory.getLogger(HttpResponse.class);
+
+	private static final long serialVersionUID = -3512681144230291786L;
