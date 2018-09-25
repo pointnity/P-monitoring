dd671dfad372b9aa2a59ffce00eade9e9c1b65ba
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-25 14:21:38 +0800

    Update HttpRequestDecoder.java

--- src/zoo/http/common/src/main/java/org/tio/http/common/HttpRequestDecoder.java
+++ src/zoo/http/common/src/main/java/org/tio/http/common/HttpRequestDecoder.java
@@ -10,11 +10,19 @@ import java.util.Map;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.tio.core.ChannelContext;
-import org.tio.core.exception.AioDecodeException;
-import org.tio.core.exception.LengthOverflowException;
-import org.tio.core.utils.ByteBufferUtils;
-import org.tio.http.common.HttpConst.RequestBodyFormat;
-import org.tio.http.common.utils.HttpParseUtils;
+import org.core.ChannelContext;
+import org.core.exception.AioDecodeException;
+import org.core.exception.LengthOverflowException;
+import org.core.utils.ByteBufferUtils;
+import org.http.common.HttpConst.RequestBodyFormat;
+import org.http.common.utils.HttpParseUtils;
 
 import com.xiaoleilu.hutool.util.StrUtil;
+
+/**
+ *
+ * @author tanyaowu
+ *
+ */
+public class HttpRequestDecoder {
+	public static enum Step {
