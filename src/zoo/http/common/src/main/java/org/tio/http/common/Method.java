f6c419f3805f2b5f0e961bb994baceb9fde29a80
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-27 14:13:55 +0800

    Update Method.java

--- src/zoo/http/common/src/main/java/org/tio/http/common/Method.java
+++ src/zoo/http/common/src/main/java/org/tio/http/common/Method.java
@@ -4,3 +4,9 @@ import java.util.Objects;
 
 /**
  * @author tanyaowu
+ *   
+ */
+public enum Method {
+	GET("GET"), POST("POST"), HEAD("HEAD"), PUT("PUT"), TRACE("TRACE"), OPTIONS("OPTIONS"), PATCH("PATCH");
+	public static Method from(String method) {
+		Method[] values = Method.values();
