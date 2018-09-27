d8581dec1770ad105343cb143ba12a43cde706f0
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-27 14:30:46 +0800

    Update RequestLine.java

--- src/zoo/http/common/src/main/java/org/tio/http/common/RequestLine.java
+++ src/zoo/http/common/src/main/java/org/tio/http/common/RequestLine.java
@@ -4,3 +4,10 @@ package org.http.common;
  * @author tanyaowu
  *  
  */
+public class RequestLine {
+	private Method method;
+	private String path; //user/get
+	private String query; //tan&id=789
+	private String pathAndQuery;
+	private String protocol;
+	private String version;
