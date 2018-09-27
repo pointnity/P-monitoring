f7b5c0cbbbdac76b8015efe461bd31616e0dc2cc
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-27 14:42:18 +0800

    Update HttpRequestHandler.java

--- src/zoo/http/common/src/main/java/org/tio/http/common/handler/HttpRequestHandler.java
+++ src/zoo/http/common/src/main/java/org/tio/http/common/handler/HttpRequestHandler.java
@@ -3,3 +3,9 @@ package org.http.common.handler;
 import org.http.common.HttpRequest;
 import org.http.common.HttpResponse;
 import org.http.common.RequestLine;
+
+/**
+ * http request processor
+ * @author tanyaowu 
+ * 
+ */
