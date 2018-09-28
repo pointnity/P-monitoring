54c1c0c89540f35fcb1ca41f022cd698e71dc24f
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-28 09:42:36 +0800

    Update HttpSession.java

--- src/zoo/http/common/src/main/java/org/tio/http/common/session/HttpSession.java
+++ src/zoo/http/common/src/main/java/org/tio/http/common/session/HttpSession.java
@@ -2,3 +2,7 @@ package org.http.common.session;
 
 import java.io.Serializable;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
