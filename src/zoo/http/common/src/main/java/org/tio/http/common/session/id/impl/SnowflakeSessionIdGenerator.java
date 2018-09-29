fa086ab451e972421a511d6c0f555b207525b780
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-29 13:21:44 +0800

    Update SnowflakeSessionIdGenerator.java

--- src/zoo/http/common/src/main/java/org/tio/http/common/session/id/impl/SnowflakeSessionIdGenerator.java
+++ src/zoo/http/common/src/main/java/org/tio/http/common/session/id/impl/SnowflakeSessionIdGenerator.java
@@ -1,3 +1,7 @@
 package org.tio.http.common.session.id.impl;
 
 import org.tio.http.common.HttpConfig;
+import org.tio.http.common.HttpRequest;
+import org.tio.http.common.session.id.ISessionIdGenerator;
+
+import com.xiaoleilu.hutool.lang.Snowflake;
