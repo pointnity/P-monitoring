5e9fd5073d8c22c08b9f333a1f77f07f075e5735
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-29 13:24:49 +0800

    Update UUIDSessionIdGenerator.java

--- src/zoo/http/common/src/main/java/org/tio/http/common/session/id/impl/UUIDSessionIdGenerator.java
+++ src/zoo/http/common/src/main/java/org/tio/http/common/session/id/impl/UUIDSessionIdGenerator.java
@@ -3,3 +3,7 @@ package org.tio.http.common.session.id.impl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.tio.http.common.HttpConfig;
+import org.tio.http.common.HttpRequest;
+import org.tio.http.common.session.id.ISessionIdGenerator;
+
+import com.xiaoleilu.hutool.util.RandomUtil;
