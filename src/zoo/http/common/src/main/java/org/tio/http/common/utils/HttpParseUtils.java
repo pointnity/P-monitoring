a6e794cf635264abc68eb7f1d93e8e74dc8e0dd3
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-09-29 13:40:09 +0800

    Update HttpParseUtils.java

--- src/zoo/http/common/src/main/java/org/tio/http/common/utils/HttpParseUtils.java
+++ src/zoo/http/common/src/main/java/org/tio/http/common/utils/HttpParseUtils.java
@@ -69,3 +69,4 @@ public class HttpParseUtils {
 	 * 	The value of the boundary is parsed directly.
 	 * 	How to use: Getperprotyequalvalue (Packetmap, "Content-type", "boundary") get ujjLiiJBznFt70fG1F4EUCkIupn7H4tzm
 	 * </pre>
+	 * @param propertyName   Property name
