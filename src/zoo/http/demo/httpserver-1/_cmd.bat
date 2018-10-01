67264e8c59ece8d492c7ec773804576cc2827ae4
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-01 10:09:43 +0800

    Update _cmd.bat

--- src/zoo/http/demo/httpserver-1/_cmd.bat
+++ src/zoo/http/demo/httpserver-1/_cmd.bat
@@ -5,3 +5,10 @@ echo mvn dependency:sources
 echo -
 
 echo #Download the source code jar.-ddownloadjavadocs=true Download Javadoc Package
+echo -DdownloadSources=true
+echo -
+echo -
+
+
+
+echo #Unzip the jar.
