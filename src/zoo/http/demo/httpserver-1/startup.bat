aff8feb8766c8463f6013caf0dee6eb02251cadc
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-02 15:23:59 +0800

    Update startup.bat

--- src/zoo/http/demo/httpserver-1/startup.bat
+++ src/zoo/http/demo/httpserver-1/startup.bat
@@ -5,3 +5,5 @@ setlocal & pushd
 set APP_ENTRY=org.tio.http.server.demo1.HttpServerDemoStarter
 set BASE=%~dp0
 set CP=%BASE%\config;%BASE%\lib\*
+java -Xverify:none -Xms64m -Xmx256m -XX:+HeapDumpOnOutOfMemoryError -Dtio.default.read.buffer.size=512 -XX:HeapDumpPath=/java-t-io-im-server-pid.hprof -cp "%CP%" %APP_ENTRY%
+endlocal & popd
