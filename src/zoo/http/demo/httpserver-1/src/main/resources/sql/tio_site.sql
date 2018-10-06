4199c78a4acac1be0057ca1571b2a6f51e86532f
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-06 14:31:17 +0800

    Update tio_site.sql

--- src/zoo/http/demo/httpserver-1/src/main/resources/sql/tio_site.sql
+++ src/zoo/http/demo/httpserver-1/src/main/resources/sql/tio_site.sql
@@ -5,3 +5,7 @@ Source Server         : 112.74.183.177
 Source Server Version : 50718
 Source Host : 112.74.183.177:3306
 Source Database : tio_site
+
+Target Server Type : MYSQL
+Target Server Version : 50718
+File Encoding : 65001
