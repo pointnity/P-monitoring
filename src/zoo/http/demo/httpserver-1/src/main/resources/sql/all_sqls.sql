e8a133e89151d00cea796bc40f5c216984f2cfd6
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-06 14:27:42 +0800

    Update all_sqls.sql

--- src/zoo/http/demo/httpserver-1/src/main/resources/sql/all_sqls.sql
+++ src/zoo/http/demo/httpserver-1/src/main/resources/sql/all_sqls.sql
@@ -1,1 +1,5 @@
 Here is the unified management of all  sql , the advantages are:
+1 : Avoid  adding sql template files one by one in JFinalClubConfig   
+2 : Exempt the  namespace from writing in the actual template file , so as not to let the  sql  definition indent back one layer.
+3 : In this file, you can also  define  some general template functions through the define directive for global sharing.
+   For example, defining a generic  CRUD  template function
