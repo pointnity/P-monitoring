1f2d80b539c195f99018470e24d6acaff769692c
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-06 14:38:59 +0800

    Update HttpServerDemoStarter.java

--- src/zoo/http/demo/httpserver-1/src/main/java/org/tio/http/server/demo1/HttpServerDemoStarter.java
+++ src/zoo/http/demo/httpserver-1/src/main/java/org/tio/http/server/demo1/HttpServerDemoStarter.java
@@ -10,3 +10,8 @@ import org.tio.http.server.demo1.init.JsonInit;
 import org.tio.http.server.demo1.init.PropInit;
 
 /**
+ * ab -c 10 -n 200000 -k http://127.0.0.1:9527/test/abtest
+ * ab -c 20 -n 200000 -k http://127.0.0.1:9527/test/abtest
+ * ab -c 40 -n 200000 -k http://127.0.0.1:9527/test/abtest
+ * ab -c 60 -n 200000 -k http://127.0.0.1:9527/test/abtest
+ * ab -c 80 -n 200000 -k http://127.0.0.1:9527/test/abtest
