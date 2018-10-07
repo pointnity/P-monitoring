a8f41dc2bd73606fef454f045d451a0449508983
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-07 15:02:44 +0800

    Update TestController.java

--- src/zoo/http/demo/httpserver-1/src/main/java/org/tio/http/server/demo1/controller/TestController.java
+++ src/zoo/http/demo/httpserver-1/src/main/java/org/tio/http/server/demo1/controller/TestController.java
@@ -4,3 +4,7 @@ import java.io.File;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.tio.http.common.HttpRequest;
+import org.tio.http.common.HttpResponse;
+import org.tio.http.common.UploadFile;
+import org.tio.http.server.annotation.RequestPath;
