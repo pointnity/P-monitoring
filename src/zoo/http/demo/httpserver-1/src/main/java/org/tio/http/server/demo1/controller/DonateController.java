e7e25ea4a462773189d446a2cc8baee208a1ecf1
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-07 14:14:21 +0800

    Update DonateController.java

--- src/zoo/http/demo/httpserver-1/src/main/java/org/tio/http/server/demo1/controller/DonateController.java
+++ src/zoo/http/demo/httpserver-1/src/main/java/org/tio/http/server/demo1/controller/DonateController.java
@@ -1,3 +1,7 @@
 package org.tio.http.server.demo1.controller;
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.tio.http.common.HttpRequest;
+import org.tio.http.common.HttpResponse;
+import org.tio.http.server.annotation.RequestPath;
