5a311c845c4371e8830a8a95721df6ef09b9c0bf
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-08 14:43:37 +0800

    Update _Generator.java

--- src/zoo/http/demo/httpserver-1/src/main/java/org/tio/http/server/demo1/model/_Generator.java
+++ src/zoo/http/demo/httpserver-1/src/main/java/org/tio/http/server/demo1/model/_Generator.java
@@ -3,3 +3,6 @@ package org.tio.http.server.demo1.model;
 import javax.sql.DataSource;
 
 import org.tio.http.server.demo1.init.JfinalInit;
+
+import com.jfinal.kit.PathKit;
+import com.jfinal.plugin.activerecord.dialect.MysqlDialect;
