d1794942ec9d4f25da21034f79d8ae23f142f00b
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-08 14:59:45 +0800

    Update _MappingKit.java

--- src/zoo/http/demo/httpserver-1/src/main/java/org/tio/http/server/demo1/model/_MappingKit.java
+++ src/zoo/http/demo/httpserver-1/src/main/java/org/tio/http/server/demo1/model/_MappingKit.java
@@ -14,3 +14,6 @@ import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
  * </pre>
  */
 public class _MappingKit {
+
+	public static void mapping(ActiveRecordPlugin arp) {
+		arp.addMapping("blog", "id", Blog.class);
