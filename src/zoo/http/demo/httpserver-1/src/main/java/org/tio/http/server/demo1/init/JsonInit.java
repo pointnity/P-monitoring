407eabd98b059e9bd1638f30a6afc2d8e9bff82a
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-08 14:29:27 +0800

    Update JsonInit.java

--- src/zoo/http/demo/httpserver-1/src/main/java/org/tio/http/server/demo1/init/JsonInit.java
+++ src/zoo/http/demo/httpserver-1/src/main/java/org/tio/http/server/demo1/init/JsonInit.java
@@ -3,3 +3,9 @@ package org.tio.http.server.demo1.init;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.tio.utils.json.Json;
+import org.tio.utils.jfinal.JfinalRecordSerializer;
+
+import com.jfinal.plugin.activerecord.Record;
+
+/**
+ * @author tanyaowu
