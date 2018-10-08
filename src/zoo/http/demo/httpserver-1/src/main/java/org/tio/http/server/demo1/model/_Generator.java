248214df6d1024f8477afdbb00d0d8efe461f26b
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-08 14:44:59 +0800

    Update _Generator.java

--- src/zoo/http/demo/httpserver-1/src/main/java/org/tio/http/server/demo1/model/_Generator.java
+++ src/zoo/http/demo/httpserver-1/src/main/java/org/tio/http/server/demo1/model/_Generator.java
@@ -17,3 +17,7 @@ public class _Generator {
 	/**
 	 * Some features are implemented using the Db + Record mode, without the need to generate the model table in this configuration
 	 */
+	private static String[] excludedTable = { "news_feed_reply", // Do not implement this feature temporarily
+			"project_page_view", "share_page_view", "feedback_page_view", "login_log", "sensitive_words", "upload_counter", "task_run_log", "message_tip", "friend", "project_like",
+			"share_like", "feedback_like", "share_reply_like", "feedback_reply_like", "like_message_log" };
+
