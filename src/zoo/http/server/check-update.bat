cd099d9047a90b90f86c5036a777b5cd676332d8
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-10 14:39:57 +0800

    Update check-update.bat

--- src/zoo/http/server/check-update.bat
+++ src/zoo/http/server/check-update.bat
@@ -18,3 +18,7 @@ Echo -
 Echo #Copy the jar to a directory (all jars are in the same directory)
 Echo mvn dependency:copy-dependencies -Dmdep.useRepositoryLayout=false
 Echo -
+
+Echo #Copy the jar to the repository directory ()
+Echo mvn dependency:copy-dependencies -Dmdep.useRepositoryLayout=true -Dmdep.copyPom=true
+Echo -
