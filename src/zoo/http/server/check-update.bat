a3bd347f35a3f464c76f2517a50cd4e7b5b5c63e
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-10 14:39:19 +0800

    Update check-update.bat

--- src/zoo/http/server/check-update.bat
+++ src/zoo/http/server/check-update.bat
@@ -1,3 +1,8 @@
 Echo off
 Echo -
 Echo #download source code
+Echo mvn dependency:sources
+Echo -
+
+Echo #download source code jar. -DdownloadJavadocs=true Download javadoc package
+Echo -DdownloadSources=true
