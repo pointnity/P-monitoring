ba352a449cc3943afb9396656ee36f85d1f5926b
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-11 12:33:46 +0800

    Create  HttpServerAioListener.java

---
+++ src/zoo/http/server/src/main/java/org/tio/http/server/HttpServerAioListener.java
@@ -0,0 +1,21 @@
+package org.tio.http.server;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.tio.core.Aio;
+import org.tio.core.ChannelContext;
+import org.tio.core.intf.Packet;
+import org.tio.http.common.HttpConst;
+import org.tio.http.common.HttpRequest;
+import org.tio.http.common.HttpResponse;
+import org.tio.server.intf.ServerAioListener;
+
+/**
+ *
+ * @author tanyaowu
+ *
+ */
