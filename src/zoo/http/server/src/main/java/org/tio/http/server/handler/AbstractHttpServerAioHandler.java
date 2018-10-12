5979bb02d90aa2c0c62ff6001064d317b9f75820
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-12 09:26:39 +0800

    Update AbstractHttpServerAioHandler.java

--- src/zoo/http/server/src/main/java/org/tio/http/server/handler/AbstractHttpServerAioHandler.java
+++ src/zoo/http/server/src/main/java/org/tio/http/server/handler/AbstractHttpServerAioHandler.java
@@ -9,3 +9,16 @@ import org.tio.core.ChannelContext;
 import org.tio.core.GroupContext;
 import org.tio.core.exception.AioDecodeException;
 import org.tio.core.intf.Packet;
+import org.tio.http.common.HttpConfig;
+import org.tio.http.common.HttpRequest;
+import org.tio.http.common.HttpRequestDecoder;
+import org.tio.http.common.HttpResponse;
+import org.tio.http.common.HttpResponseEncoder;
+import org.tio.http.common.handler.HttpRequestHandler;
+import org.tio.http.server.mvc.Routes;
+import org.tio.server.intf.ServerAioHandler;
+
+/**
+ *
+ * @author tanyaowu
+ *
