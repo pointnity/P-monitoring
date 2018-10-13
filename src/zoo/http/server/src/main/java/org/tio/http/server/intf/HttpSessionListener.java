a9b2ace9ce183a3449ca50ddc51f7a8952ee0d35
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-13 14:54:42 +0800

    Update HttpSessionListener.java

--- src/zoo/http/server/src/main/java/org/tio/http/server/intf/HttpSessionListener.java
+++ src/zoo/http/server/src/main/java/org/tio/http/server/intf/HttpSessionListener.java
@@ -7,3 +7,12 @@ import org.tio.http.common.session.HttpSession;
  * @author tanyaowu 
  *  
  */
+public interface HttpSessionListener {
+	/**
+	 * 
+	 * @param session
+	 * @author: tanyaowu
+	 */
+	public void doAfterCreated(HttpSession session, HttpConfig httpConfig);
+
+}
