2b947790cacbdf4ceff998f1af49f52b883aaa53
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-13 14:55:51 +0800

    Create   Routes.java

---
+++ src/zoo/http/server/src/main/java/org/tio/http/server/mvc/Routes.java
@@ -0,0 +1,26 @@
+package org.tio.http.server.mvc;
+
+import java.lang.reflect.Executable;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.tio.http.server.annotation.RequestPath;
+import org.tio.utils.json.Json;
+
+import com.thoughtworks.paranamer.BytecodeReadingParanamer;
+import com.thoughtworks.paranamer.Paranamer;
+import com.xiaoleilu.hutool.util.ArrayUtil;
+
+import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner;
+import io.github.lukehutch.fastclasspathscanner.matchprocessor.ClassAnnotationMatchProcessor;
+import io.github.lukehutch.fastclasspathscanner.matchprocessor.MethodAnnotationMatchProcessor;
+
+/**
+ * @author tanyaowu
+ *  
+ */
