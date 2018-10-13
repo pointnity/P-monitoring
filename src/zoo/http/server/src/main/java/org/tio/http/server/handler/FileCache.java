f1661ca8bf0630533a13012487d023f30093c525
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-13 14:48:57 +0800

    Update FileCache.java

--- src/zoo/http/server/src/main/java/org/tio/http/server/handler/FileCache.java
+++ src/zoo/http/server/src/main/java/org/tio/http/server/handler/FileCache.java
@@ -9,3 +9,22 @@ import org.slf4j.LoggerFactory;
  * @author tanyaowu
  *  
  */
+public class FileCache implements java.io.Serializable {
+
+	private static final long serialVersionUID = 6517890350387789902L;
+
+	private static Logger log = LoggerFactory.getLogger(FileCache.class);
+
+	/**
+	 * @param args
+	 * @author tanyaowu
+	 */
+	public static void main(String[] args) {
+
+	}
+
+	//this.addHeader(HttpConst.ResponseHeaderKey.Content_Encoding, "gzip");
+	private Map<String, String> headers = null;
+	private long lastModified;
+
+	private byte[] data;
