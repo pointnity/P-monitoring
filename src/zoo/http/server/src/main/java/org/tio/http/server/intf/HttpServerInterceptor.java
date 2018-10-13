2b3b70e95f2756fb5cea0f1918d5e8b3a25dbbf9
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-13 14:50:20 +0800

    Create   HttpServerInterceptor.java

---
+++ src/zoo/http/server/src/main/java/org/tio/http/server/intf/HttpServerInterceptor.java
@@ -0,0 +1,10 @@
+package org.tio.http.server.intf;
+
+import org.tio.http.common.HttpRequest;
+import org.tio.http.common.HttpResponse;
+import org.tio.http.common.RequestLine;
+
+/**
+ * @author tanyaowu
+ *  
+ */
