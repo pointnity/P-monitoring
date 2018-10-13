e7a702fdfa9430d8082d1b945975909fb9b5a59c
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-13 14:25:08 +0800

    Create  DefaultHttpRequestHandler.java

---
+++ src/zoo/http/server/src/main/java/org/tio/http/server/handler/DefaultHttpRequestHandler.java
@@ -0,0 +1,9 @@
+package org.tio.http.server.handler;
+
+import java.beans.PropertyDescriptor;
+import java.io.File;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
