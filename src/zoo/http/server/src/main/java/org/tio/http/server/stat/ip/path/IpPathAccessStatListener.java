cfb684f78e4969bca3115aae6c239d30181c2103
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-14 10:16:58 +0800

    Create IpPathAccessStatListener.java

---
+++ src/zoo/http/server/src/main/java/org/tio/http/server/stat/ip/path/IpPathAccessStatListener.java
@@ -0,0 +1,11 @@
+package org.tio.http.server.stat.ip.path;
+
+import org.tio.core.GroupContext;
+import org.tio.http.common.HttpRequest;
+
+public interface IpPathAccessStatListener {
+
+	/**
+	 * 
+	 * @param groupContext
+	 * @param ip
