b96c28da7e9e93aa46f8a3e5915c53078f65a98d
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-14 10:00:12 +0800

    Update SessionCookieDecorator.java

--- src/zoo/http/server/src/main/java/org/tio/http/server/session/SessionCookieDecorator.java
+++ src/zoo/http/server/src/main/java/org/tio/http/server/session/SessionCookieDecorator.java
@@ -6,3 +6,7 @@ import org.tio.http.common.Cookie;
  * @author tanyaowu 
  *  
  */
+public interface SessionCookieDecorator {
+	/**
+	 * DefaultHttpRequestHandlerA cookie is created for the session based on the host field, and the user can customize the cookie by using this method.
+	 * For example, the domain name of the cookie is www.Baidu.COM changed to.Baidu.Com
