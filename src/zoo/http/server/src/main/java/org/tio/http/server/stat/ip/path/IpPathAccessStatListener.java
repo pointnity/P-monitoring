74122906591d98009c6d80ca9689c628d5f8028c
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-14 10:17:12 +0800

    Update IpPathAccessStatListener.java

--- src/zoo/http/server/src/main/java/org/tio/http/server/stat/ip/path/IpPathAccessStatListener.java
+++ src/zoo/http/server/src/main/java/org/tio/http/server/stat/ip/path/IpPathAccessStatListener.java
@@ -9,3 +9,21 @@ public interface IpPathAccessStatListener {
 	 * 
 	 * @param groupContext
 	 * @param ip
+	 * @param ipAccessStat
+	 * @author tanyaowu
+	 */
+	public void onExpired(GroupContext groupContext, String ip, IpAccessStat ipAccessStat);
+	
+	/**
+	 * 
+	 * @param httpRequest
+	 * @param ip
+	 * @param path
+	 * @param ipAccessStat
+	 * @param ipPathAccessStat
+	 * @author tanyaowu
+	 */
+	public boolean onChanged(HttpRequest httpRequest, String ip, String path, IpAccessStat ipAccessStat, IpPathAccessStat ipPathAccessStat);
+
+
+}
