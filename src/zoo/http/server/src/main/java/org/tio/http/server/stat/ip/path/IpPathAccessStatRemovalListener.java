8ee806424f82bcece0b34827df9d222c1fdba6d7
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-14 10:18:14 +0800

    Update IpPathAccessStatRemovalListener.java

--- src/zoo/http/server/src/main/java/org/tio/http/server/stat/ip/path/IpPathAccessStatRemovalListener.java
+++ src/zoo/http/server/src/main/java/org/tio/http/server/stat/ip/path/IpPathAccessStatRemovalListener.java
@@ -12,3 +12,29 @@ import com.google.common.cache.RemovalNotification;
  *  
  */
 @SuppressWarnings("rawtypes")
+public class IpPathAccessStatRemovalListener implements RemovalListener {
+	@SuppressWarnings("unused")
+	private static Logger log = LoggerFactory.getLogger(IpPathAccessStatRemovalListener.class);
+
+	private IpPathAccessStatListener ipPathAccessStatListener;
+
+	private GroupContext groupContext = null;
+
+	/**
+	 * 
+	 * @author: tanyaowu
+	 */
+	public IpPathAccessStatRemovalListener(GroupContext groupContext, IpPathAccessStatListener ipPathAccessStatListener) {
+		this.groupContext = groupContext;
+		this.ipPathAccessStatListener = ipPathAccessStatListener;
+	}
+
+	/**
+	 * @param args
+	 * @author: tanyaowu
+	 */
+	public static void main(String[] args) {
+
+	}
+
+	@Override
