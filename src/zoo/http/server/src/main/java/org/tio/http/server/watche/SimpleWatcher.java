a333504e1dedee0c1dd446e1e1bcd982a1e29b70
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-15 09:27:56 +0800

    Create   SimpleWatcher.java

---
+++ src/zoo/http/server/src/main/java/org/tio/http/server/watche/SimpleWatcher.java
@@ -0,0 +1,14 @@
+package org.tio.http.server.watche;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.WatchEvent;
+
+import com.xiaoleilu.hutool.io.watch.WatchMonitor;
+import com.xiaoleilu.hutool.io.watch.Watcher;
+
+/**
+ *
+ * @author tanyaowu
+ *  
+ */
