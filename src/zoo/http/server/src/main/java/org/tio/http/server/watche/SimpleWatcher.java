9effb4d51eaf60a0034dfe7624ce27c59342267e
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-15 09:28:11 +0800

    Update SimpleWatcher.java

--- src/zoo/http/server/src/main/java/org/tio/http/server/watche/SimpleWatcher.java
+++ src/zoo/http/server/src/main/java/org/tio/http/server/watche/SimpleWatcher.java
@@ -12,3 +12,19 @@ import com.xiaoleilu.hutool.io.watch.Watcher;
  * @author tanyaowu
  *  
  */
+public class SimpleWatcher implements Watcher {
+
+	public static void main(String[] args) {
+		WatchMonitor watchMonitor = WatchMonitor.create(Paths.get("c://"), 1000000, new WatchEvent.Kind() {
+
+			@Override
+			public String name() {
+				return null;
+			}
+
+			@Override
+			public Class<?> type() {
+				return null;
+			}
+		});
+		SimpleWatcher simpleWatcher = new SimpleWatcher();
