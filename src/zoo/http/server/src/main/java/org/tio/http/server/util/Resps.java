adb23fa8f27904a613a046ae30dd8401bb5274b0
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-15 09:12:57 +0800

    Create  Resps.java

---
+++ src/zoo/http/server/src/main/java/org/tio/http/server/util/Resps.java
@@ -0,0 +1,11 @@
+package org.tio.http.server.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
