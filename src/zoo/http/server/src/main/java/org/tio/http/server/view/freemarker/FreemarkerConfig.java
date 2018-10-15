542b63735b8a1885c0fc1caf516a8591ef7f23b1
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-15 09:24:40 +0800

    Create FreemarkerConfig.java

---
+++ src/zoo/http/server/src/main/java/org/tio/http/server/view/freemarker/FreemarkerConfig.java
@@ -0,0 +1,11 @@
+package org.tio.http.server.view.freemarker;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import freemarker.template.Configuration;
+
+/**
+ * @author tanyaowu 
+ *  
+ */
