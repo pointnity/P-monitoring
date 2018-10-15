3a6192c2fe7f717fbe42ba02b4bd23524cb0bffa
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-15 09:24:53 +0800

    Update FreemarkerConfig.java

--- src/zoo/http/server/src/main/java/org/tio/http/server/view/freemarker/FreemarkerConfig.java
+++ src/zoo/http/server/src/main/java/org/tio/http/server/view/freemarker/FreemarkerConfig.java
@@ -9,3 +9,22 @@ import freemarker.template.Configuration;
  * @author tanyaowu 
  *  
  */
+public class FreemarkerConfig {
+	private static Logger log = LoggerFactory.getLogger(FreemarkerConfig.class);
+	
+	private Configuration configuration;
+	
+	private ModelMaker modelMaker;
+	
+	private String[] suffixes = null;
+	
+	public FreemarkerConfig(Configuration configuration, ModelMaker modelMaker, String[] suffixes) {
+		super();
+		this.configuration = configuration;
+		this.modelMaker = modelMaker;
+		this.setSuffixes(suffixes);
+	}
+
+	
+
+	public Configuration getConfiguration() {
