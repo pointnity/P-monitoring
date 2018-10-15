b4b769be738d99593ede6894dab953fa3ca294ca
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-15 09:08:03 +0800

    Create  HttpServerUtils.java

---
+++ src/zoo/http/server/src/main/java/org/tio/http/server/util/HttpServerUtils.java
@@ -0,0 +1,14 @@
+package org.tio.http.server.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.tio.core.ChannelContext;
+import org.tio.core.GroupContext;
+import org.tio.http.common.GroupContextKey;
+import org.tio.http.common.HttpConfig;
+import org.tio.http.common.HttpRequest;
+
+/**
+ * @author tanyaowu
+ * 
+ */
