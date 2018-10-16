0a9355f167544bddb13e896fa19781b3c7708c78
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-16 11:41:01 +0800

    Create _cmd.bat

---
+++ src/zoo/parent/_cmd.bat
@@ -0,0 +1,10 @@
+Echo off
+Echo -
+Echo #download source code
+Echo mvn dependency:sources
+Echo -
+
+Echo #download source code jar. -DdownloadJavadocs=true Download javadoc package
+Echo -DdownloadSources=true
+Echo -
+Echo -
