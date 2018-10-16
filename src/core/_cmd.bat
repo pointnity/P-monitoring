87b906c4515b51ee1cb31afd1fd943f1bf617360
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-16 11:53:33 +0800

    Create _cmd.bat

---
+++ src/core/_cmd.bat
@@ -0,0 +1,9 @@
+Echo off
+Echo -
+Echo #download source code
+Echo mvn dependency:sources
+Echo -
+
+Echo #download source code jar. -DdownloadJavadocs=true Download javadoc package
+Echo -DdownloadSources=true
+Echo -
