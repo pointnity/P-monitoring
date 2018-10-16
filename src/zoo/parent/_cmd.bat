d511d8424fd832145f0c1b2fdbfd9ba611e3b1ef
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-16 11:41:22 +0800

    Update _cmd.bat

--- src/zoo/parent/_cmd.bat
+++ src/zoo/parent/_cmd.bat
@@ -8,3 +8,22 @@ Echo #download source code jar. -DdownloadJavadocs=true Download javadoc package
 Echo -DdownloadSources=true
 Echo -
 Echo -
+
+
+
+Echo # jar out
+Echo mvn dependency:unpack-dependencies
+Echo -
+
+Echo #Copy the jar to a directory (all jars are in the same directory)
+Echo mvn dependency:copy-dependencies -Dmdep.useRepositoryLayout=false
+Echo -
+
+Echo #Copy the jar to the repository directory ()
+Echo mvn dependency:copy-dependencies -Dmdep.useRepositoryLayout=true -Dmdep.copyPom=true
+Echo -
+Echo -
+
+
+
+Echo #check version update
