8f59f2991a14f2584ff4d970dc1f10ea90ba9c20
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-17 14:14:37 +0800

    Create ClientGroupContext.java

---
+++ src/core/src/main/java/org/tio/client/ClientGroupContext.java
@@ -0,0 +1,16 @@
+package org.tio.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.tio.client.intf.ClientAioHandler;
+import org.tio.client.intf.ClientAioListener;
+import org.tio.core.GroupContext;
+import org.tio.core.intf.AioHandler;
+import org.tio.core.intf.AioListener;
+import org.tio.core.stat.GroupStat;
+
+/**
+ *
+ * @author tanyaowu
+ *  
+ */
