4bb258901b57b01ab02853ffdb68116bf50b0c89
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-17 14:06:44 +0800

    Create ClientChannelContext.java

---
+++ src/core/src/main/java/org/tio/client/ClientChannelContext.java
@@ -0,0 +1,8 @@
+package org.tio.client;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.AsynchronousSocketChannel;
+
+import org.tio.core.ChannelContext;
+import org.tio.core.GroupContext;
