f44bedcfc5b58d8be524a1a742cf8e3e4acbbfdf
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-18 09:59:35 +0800

    Update ClientGroupStat.java

--- src/core/src/main/java/org/tio/client/ClientGroupStat.java
+++ src/core/src/main/java/org/tio/client/ClientGroupStat.java
@@ -4,3 +4,10 @@ import org.tio.core.stat.GroupStat;
 
 /**
  *
+* @author tanyaowu
+ *  
+ */
+public class ClientGroupStat extends GroupStat {
+
+	private static final long serialVersionUID = 804270967157603097L;
+}
