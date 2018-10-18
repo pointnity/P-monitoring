96fced5ddeca37c710f617eaf3b080daf82ca31f
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-18 10:19:54 +0800

    Update ClientAioListener.java

--- src/core/src/main/java/org/tio/client/intf/ClientAioListener.java
+++ src/core/src/main/java/org/tio/client/intf/ClientAioListener.java
@@ -7,3 +7,7 @@ import org.tio.core.intf.AioListener;
  * @author tanyaowu
  *  
  */
+public interface ClientAioListener extends AioListener {
+
+	/**
+	 * This method is triggered after a re-connection
