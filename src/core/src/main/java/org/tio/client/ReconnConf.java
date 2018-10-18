f624a81aae8e859682756af9b0a5a5569d398e15
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-18 10:10:00 +0800

    Create  ReconnConf.java

---
+++ src/core/src/main/java/org/tio/client/ReconnConf.java
@@ -0,0 +1,8 @@
+package org.tio.client;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
