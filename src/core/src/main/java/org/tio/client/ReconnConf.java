bf80efc200d94c19d5f54302d3336ecca45cd9c2
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-18 10:10:18 +0800

    Update ReconnConf.java

--- src/core/src/main/java/org/tio/client/ReconnConf.java
+++ src/core/src/main/java/org/tio/client/ReconnConf.java
@@ -6,3 +6,10 @@ import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.tio.core.ChannelContext;
+import org.tio.utils.SystemTimer;
+import org.tio.utils.thread.pool.DefaultThreadFactory;
+
+/**
+ *
+ * @author tanyaowu
