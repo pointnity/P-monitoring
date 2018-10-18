737f2ec3abcd51c86ec9793a24c2563d24d63603
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-18 10:01:23 +0800

    Update ConnectionCompletionHandler.java

--- src/core/src/main/java/org/tio/client/ConnectionCompletionHandler.java
+++ src/core/src/main/java/org/tio/client/ConnectionCompletionHandler.java
@@ -44,3 +44,21 @@ public class ConnectionCompletionHandler implements CompletionHandler<Void, Conn
 	/**
 	 *
 	 * @param result
+	 * @param attachment
+	 * @param throwable
+	 * @author tanyaowu
+	 */
+	private void handler(Void result, ConnectionCompletionVo attachment, Throwable throwable) {
+		ClientChannelContext channelContext = attachment.getChannelContext();
+		AsynchronousSocketChannel asynchronousSocketChannel = attachment.getAsynchronousSocketChannel();
+		AioClient aioClient = attachment.getAioClient();
+		ClientGroupContext clientGroupContext = aioClient.getClientGroupContext();
+		Node serverNode = attachment.getServerNode();
+		String bindIp = attachment.getBindIp();
+		Integer bindPort = attachment.getBindPort();
+		ClientAioListener clientAioListener = clientGroupContext.getClientAioListener();
+		boolean isReconnect = attachment.isReconnect();
+		boolean isConnected = false;
+
+		try {
+			if (throwable == null) {
