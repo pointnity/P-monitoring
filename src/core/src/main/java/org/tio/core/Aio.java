f27f051aa91f88b7181c4fc71a5aa2fc52efb562
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-18 10:24:01 +0800

    Create Aio.java

---
+++ src/core/src/main/java/org/tio/core/Aio.java
@@ -0,0 +1,17 @@
+package org.tio.core;
+
+import java.nio.ByteBuffer;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock.ReadLock;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.tio.core.intf.Packet;
+import org.tio.core.intf.PacketWithMeta;
