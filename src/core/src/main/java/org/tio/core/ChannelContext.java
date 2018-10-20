219d75306e6144fb5731e2d15c8a4d8bdca17ec2
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-20 10:04:41 +0800

    Update ChannelContext.java

--- src/core/src/main/java/org/tio/core/ChannelContext.java
+++ src/core/src/main/java/org/tio/core/ChannelContext.java
@@ -14,3 +14,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.tio.core.intf.Packet;
+import org.tio.core.intf.PacketWithMeta;
+import org.tio.core.stat.ChannelStat;
+import org.tio.core.task.DecodeRunnable;
+import org.tio.core.task.HandlerRunnable;
+import org.tio.core.task.SendRunnable;
+import org.tio.utils.json.Json;
+import org.tio.utils.prop.MapWithLockPropSupport;
+
+import com.xiaoleilu.hutool.date.DatePattern;
+import com.xiaoleilu.hutool.date.DateTime;
+
+/**
+ * 
+ * @author tanyaowu 
+ *  
+ */
+public abstract class ChannelContext extends MapWithLockPropSupport {
+	private static Logger log = LoggerFactory.getLogger(ChannelContext.class);
+
+	private static final String DEFAULT_ATTUBITE_KEY = "t-io-d-a-k";
+
+	public static final String UNKNOWN_ADDRESS_IP = "$UNKNOWN";
