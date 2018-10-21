a05296b25b2602b99dcf877e0209fdf09583d239
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-21 15:55:24 +0800

    Update CloseRunnable.java

--- src/core/src/main/java/org/tio/core/CloseRunnable.java
+++ src/core/src/main/java/org/tio/core/CloseRunnable.java
@@ -17,3 +17,18 @@ import org.tio.utils.SystemTimer;
  * @author tanyaowu 
  *  
  */
+public class CloseRunnable implements Runnable {
+
+	private static Logger log = LoggerFactory.getLogger(CloseRunnable.class);
+
+	private ChannelContext channelContext;
+	private Throwable throwable;
+	private String remark;
+	private boolean isNeedRemove;
+
+	/**
+	 *
+	 *
+	 * @author tanyaowu
+	 *  
+	 *
