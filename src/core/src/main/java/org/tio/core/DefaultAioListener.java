1b40efa8fefbdf8b4d7b5c08895f57d249824b12
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-21 16:12:15 +0800

    Create DefaultAioListener.java

---
+++ src/core/src/main/java/org/tio/core/DefaultAioListener.java
@@ -0,0 +1,13 @@
+package org.tio.core;
+
+import org.tio.client.intf.ClientAioListener;
+import org.tio.core.intf.Packet;
+import org.tio.server.intf.ServerAioListener;
+
+/**
+ *
+ * @author tanyaowu
+ */
+public class DefaultAioListener implements ClientAioListener, ServerAioListener {
+	/**
+	 *
