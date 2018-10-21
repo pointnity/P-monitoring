818b10127997c48e673da334bae61aee058cf833
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-21 16:14:54 +0800

    Create  DefaultChannelTraceHandler.java

---
+++ src/core/src/main/java/org/tio/core/DefaultChannelTraceHandler.java
@@ -0,0 +1,18 @@
+package org.tio.core;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.tio.core.intf.ChannelTraceHandler;
+import org.tio.core.intf.Packet;
+import org.tio.utils.json.Json;
+
+import com.xiaoleilu.hutool.date.DatePattern;
+import com.xiaoleilu.hutool.date.DateTime;
+
+/**
+ * @author tanyaowu
+ *  
