51c3b7f433581d0e872f1dea11533cf9d2db55e0
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-21 16:19:24 +0800

    Update DefaultTioUuid.java

--- src/core/src/main/java/org/tio/core/DefaultTioUuid.java
+++ src/core/src/main/java/org/tio/core/DefaultTioUuid.java
@@ -5,3 +5,14 @@ import org.tio.core.intf.TioUuid;
 /**
  * @author tanyaowu
  *  
+ */
+public class DefaultTioUuid implements TioUuid {
+	//	private static Logger log = LoggerFactory.getLogger(DefaultTioUuid.class);
+
+	/**
+	 * @param args
+	 * @author tanyaowu
+	 */
+	public static void main(String[] args) {
+
+	}
