18928bce28681c782605b5108bd15d287cfd9f17
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-23 16:15:30 +0800

    Update PacketSendMode.java

--- src/core/src/main/java/org/tio/core/PacketSendMode.java
+++ src/core/src/main/java/org/tio/core/PacketSendMode.java
@@ -10,3 +10,7 @@ public enum PacketSendMode {
 	 *Throw the packet in a queue and let the thread pool send
 	 */
 	QUEUE(1),
+	/**
+	 * A single message blocked send, the direct send succeeds, only then retrace
+	 */
+	SINGLE_BLOCK(2),
