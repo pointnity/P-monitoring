36bdc6ebf78068b7513669fb1c425bf833b6ff39
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-23 16:24:19 +0800

    Create WriteCompletionHandler.java

---
+++ src/core/src/main/java/org/tio/core/WriteCompletionHandler.java
@@ -0,0 +1,9 @@
+package org.tio.core;
+
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+import java.util.List;
+import java.util.concurrent.Semaphore;
+
+import org.slf4j.Logger;
