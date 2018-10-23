8d2695d5d30662c7719e0d9964300916ffbbe304
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-23 16:16:55 +0800

    Create ReadCompletionHandler.java

---
+++ src/core/src/main/java/org/tio/core/ReadCompletionHandler.java
@@ -0,0 +1,14 @@
+package org.tio.core;
+
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.tio.core.stat.IpStat;
+import org.tio.core.task.DecodeRunnable;
+import org.tio.core.utils.AioUtils;
