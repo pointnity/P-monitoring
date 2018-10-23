ad3608395adb33ba6d74acdfba4870dda0ecdf2f
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-23 16:15:13 +0800

    Update PacketSendMode.java

--- src/core/src/main/java/org/tio/core/PacketSendMode.java
+++ src/core/src/main/java/org/tio/core/PacketSendMode.java
@@ -4,3 +4,9 @@ package org.tio.core;
  *Message sending mode
  * @author tanyaowu
  *
+ */
+public enum PacketSendMode {
+	/**
+	 *Throw the packet in a queue and let the thread pool send
+	 */
+	QUEUE(1),
