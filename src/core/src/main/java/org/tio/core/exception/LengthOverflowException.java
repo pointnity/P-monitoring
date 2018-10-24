4401d8141fa0e996afb83967b269b9bd016a151e
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-24 11:49:39 +0800

    Update LengthOverflowException.java

--- src/core/src/main/java/org/tio/core/exception/LengthOverflowException.java
+++ src/core/src/main/java/org/tio/core/exception/LengthOverflowException.java
@@ -5,3 +5,12 @@
  * @author tanyaowu 
  *  
  */
+public class LengthOverflowException extends java.lang.Throwable {
+
+	/**
+	 * @Meaning:
+	 * @Type: Long
+	 */
+	private static final long serialVersionUID = 5231789012657669073L;
+
+	/**
