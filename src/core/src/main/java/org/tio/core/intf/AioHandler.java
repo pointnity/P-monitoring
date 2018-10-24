35570f2f345c033622653bf448c923c430c4fd65
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-24 11:52:11 +0800

    Create   AioHandler.java

---
+++ src/core/src/main/java/org/tio/core/intf/AioHandler.java
@@ -0,0 +1,10 @@
+package org.tio.core.intf;
+
+import java.nio.ByteBuffer;
+
+import org.tio.core.ChannelContext;
+import org.tio.core.GroupContext;
+import org.tio.core.exception.AioDecodeException;
+
+/**
+ * 
