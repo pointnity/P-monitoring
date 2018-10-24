a3a86f52d16c091109c66bc36de7006c4ac34a20
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-24 11:53:20 +0800

    Update AioHandler.java

--- src/core/src/main/java/org/tio/core/intf/AioHandler.java
+++ src/core/src/main/java/org/tio/core/intf/AioHandler.java
@@ -14,3 +14,6 @@ import org.tio.core.exception.AioDecodeException;
 public interface AioHandler {
 
 	/**
+	 * Packet objects that are decoded into business according to Bytebuffer.
+	 * If the received data is incomplete, causing the decoding to fail, return NULL, and the framework layer will automatically renew the previous received data when the next message comes
+	 * @param buffer
