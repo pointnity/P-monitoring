43fcd81fbda9d900843330dac7df314802e41322
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-24 11:56:35 +0800

    Update AioListener.java

--- src/core/src/main/java/org/tio/core/intf/AioListener.java
+++ src/core/src/main/java/org/tio/core/intf/AioListener.java
@@ -7,3 +7,13 @@ Import  org.tio.core.ChannelContext ;
  * @author tanyaowu
  *  
  */
+Public  interface  AioListener  {
+	/**
+	 * This method is triggered before and after the connection is closed.
+	 *
+	 * @param channelContext the channelcontext
+	 * @param throwable the throwable may be empty
+	 * @param remark the remark may be empty
+	 * @param isRemove Is it deleted?
+	 * @throws Exception
+	 * @author: tanyaowu
