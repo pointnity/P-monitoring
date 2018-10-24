36c49b14a69b618d044de9aedeb2d0f4a569a3ce
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-24 11:46:59 +0800

    Update AioDecodeException.java

--- src/core/src/main/java/org/tio/core/exception/AioDecodeException.java
+++ src/core/src/main/java/org/tio/core/exception/AioDecodeException.java
@@ -11,3 +11,15 @@ public class AioDecodeException extends java.lang.Throwable {
 	 * @Meaning:
 	 * @Type: Long
 	 */
+	private static final long serialVersionUID = 5231789012657669073L;
+
+	/**
+	 *
+	 *
+	 * @author tanyaowu
+	 *
+	 */
+	public AioDecodeException() {
+	}
+
+	/**
