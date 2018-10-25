654becb24b1d4904a9f86b859de119e0a03ad7bd
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-25 15:31:36 +0800

    Create   ChannelContextMapWithLock.java

---
+++ src/core/src/main/java/org/tio/core/maintain/ChannelContextMapWithLock.java
@@ -0,0 +1,10 @@
+package org.tio.core.maintain;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+
+import org.tio.core.intf.Packet;
+import org.tio.utils.lock.MapWithLock;
+
+/**
