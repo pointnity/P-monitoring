57b709b1aa37a79fafdc771c482d3fb1c9fb00aa
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-25 15:18:26 +0800

    Update Packet.java

--- src/core/src/main/java/org/tio/core/intf/Packet.java
+++ src/core/src/main/java/org/tio/core/intf/Packet.java
@@ -4,3 +4,12 @@ import java.nio.ByteBuffer;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
+*
+* @author tanyaowu
+* 
+*/
+public class Packet implements java.io.Serializable {
+
+	private static final long serialVersionUID = 5275372187150637318L;
+
+	private static final AtomicLong ID_ATOMICLONG = new AtomicLong();
