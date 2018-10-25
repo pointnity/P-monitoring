8998a5f99a0a749cbba43b29c7d1e7b7daf4c31a
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-25 15:27:49 +0800

    Update PacketWithMeta.java

--- src/core/src/main/java/org/tio/core/intf/PacketWithMeta.java
+++ src/core/src/main/java/org/tio/core/intf/PacketWithMeta.java
@@ -6,3 +6,15 @@ import java.util.concurrent.CountDownLatch;
  *
  * @author tanyaowu
  *  
+ */
+public class PacketWithMeta {
+	private Packet packet = null;
+	private Boolean isSentSuccess = null;
+	private CountDownLatch countDownLatch = null;
+
+	/**
+	 *
+	 * @param packet
+	 * @param countDownLatch
+	 * @author tanyaowu
+	 */
