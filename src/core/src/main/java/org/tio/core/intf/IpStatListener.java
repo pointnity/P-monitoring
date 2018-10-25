7c0b293268b0f1bc36daa732009ddada4b6b50c9
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-25 15:16:47 +0800

    Update IpStatListener.java

--- src/core/src/main/java/org/tio/core/intf/IpStatListener.java
+++ src/core/src/main/java/org/tio/core/intf/IpStatListener.java
@@ -6,3 +6,8 @@ import org.tio.core.stat.IpStat;
 /**
  * @author tanyaowu 
  *  
+ */
+public interface IpStatListener {
+	/**
+	 * After the statistics period expires, the user can implement in this method to put the relevant data into the library or log, etc.
+	 * @param ipStat
