4843c92d6fcd0ee6ea73cad1c1a5b0e7e1743bd5
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-25 15:31:46 +0800

    Update ChannelContextMapWithLock.java

--- src/core/src/main/java/org/tio/core/maintain/ChannelContextMapWithLock.java
+++ src/core/src/main/java/org/tio/core/maintain/ChannelContextMapWithLock.java
@@ -8,3 +8,13 @@ import org.tio.core.intf.Packet;
 import org.tio.utils.lock.MapWithLock;
 
 /**
+ * @author tanyaowu
+ */
+public class ChannelContextMapWithLock {
+
+	/** remoteAndChannelContext key: "ip:port" value: ChannelContext. */
+	private MapWithLock<Integer, Packet> map = new MapWithLock<>(new HashMap<Integer, Packet>());
+
+	/**
+	 * Gets the cacheMap.
+	 *
