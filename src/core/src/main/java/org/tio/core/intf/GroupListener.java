462ee45249cf174b50fd9f1d15558ae7de4b6122
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-25 15:15:11 +0800

    Update GroupListener.java

--- src/core/src/main/java/org/tio/core/intf/GroupListener.java
+++ src/core/src/main/java/org/tio/core/intf/GroupListener.java
@@ -9,3 +9,11 @@ import org.tio.core.ChannelContext;
 public interface GroupListener {
 	/**
 	 * Callback This method after binding a group
+	 * @param channelContext
+	 * @param group
+	 * @throws Exception
+	 * @author tanyaowu
+	 */
+	void onAfterBind(ChannelContext channelContext, String group) throws Exception;
+
+	/**
