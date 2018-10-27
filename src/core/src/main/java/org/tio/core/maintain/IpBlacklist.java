e6c72eda7a234fdec87cd30669327ffe25fa311a
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-27 14:46:04 +0800

    Update IpBlacklist.java

--- src/core/src/main/java/org/tio/core/maintain/IpBlacklist.java
+++ src/core/src/main/java/org/tio/core/maintain/IpBlacklist.java
@@ -13,3 +13,21 @@ import org.tio.utils.time.Time;
  * @author tanyaowu
  *  
  */
+public class IpBlacklist {
+
+	/** remoteAndChannelContext key: "ip:port" value: ChannelContext. */
+//	private SetWithLock<String> setWithLock = new SetWithLock<>(new HashSet<String>());
+
+	
+	private String id;
+
+	private final static String CACHE_NAME = "TIO_IP_BLACK_LIST";
+	private final static Long TIME_TO_LIVE_SECONDS = Time.DAY_1;
+	private final static Long TIME_TO_IDLE_SECONDS = null;
+
+	private String cacheName = null;
+	private GuavaCache cache = null;
+	
+	private GroupContext groupContext;
+
+	public IpBlacklist(String id, GroupContext groupContext) {
