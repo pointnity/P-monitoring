943ff724e72930f07baeaba9bf979d260e42dc82
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-27 14:43:32 +0800

    Create  Ids.java

---
+++ src/core/src/main/java/org/tio/core/maintain/Ids.java
@@ -0,0 +1,16 @@
+package org.tio.core.maintain;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+
+import org.apache.commons.lang3.StringUtils;
+import org.tio.core.ChannelContext;
+import org.tio.core.GroupContext;
+import org.tio.utils.lock.MapWithLock;
+
+/**
+ *
+ * @author tanyaowu
+ * 
+ */
