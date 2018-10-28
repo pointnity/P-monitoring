390a1b7f33d6ac0dcadfe474260a8dbcd9c8c964
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-28 15:41:03 +0800

    Create  MaintainUtils.java

---
+++ src/core/src/main/java/org/tio/core/maintain/MaintainUtils.java
@@ -0,0 +1,23 @@
+package org.tio.core.maintain;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.tio.core.Aio;
+import org.tio.core.ChannelContext;
+import org.tio.core.GroupContext;
+
+/**
+ * 
+ * @author tanyaowu 
+ *  
+ */
+public class MaintainUtils {
+	private static Logger log = LoggerFactory.getLogger(MaintainUtils.class);
+
+	/**
+	 * Completely removed, no longer maintained
+	 * @param channelContext
+	 *
+	 * @author tanyaowu
+	 *
