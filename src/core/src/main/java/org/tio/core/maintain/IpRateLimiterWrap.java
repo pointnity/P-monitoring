5e1368400df5cffa3faa74ab8b77a8f1f30a73e6
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-28 15:28:08 +0800

    Create  IpRateLimiterWrap.java

---
+++ src/core/src/main/java/org/tio/core/maintain/IpRateLimiterWrap.java
@@ -0,0 +1,13 @@
+package org.tio.core.maintain;
+
+/**
+ * @author tanyaowu
+ *  
+ */
+public class IpRateLimiterWrap {
+	//	private static Logger log = LoggerFactory.getLogger(IpRateLimiterWrap.class);
+
+	/**
+	 * @param args
+	 * @author tanyaowu
+	 */
