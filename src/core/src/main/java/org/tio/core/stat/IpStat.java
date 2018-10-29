9e9d8425bf616bd910ccd660415918be29bc041f
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-29 16:43:55 +0800

    Create IpStat.java

---
+++ src/core/src/main/java/org/tio/core/stat/IpStat.java
@@ -0,0 +1,16 @@
+Package  org . tio . core . stat ;
+
+Import  java.util.Date ;
+Import  java.util.concurrent.atomic.AtomicInteger ;
+Import  java.util.concurrent.atomic.AtomicLong ;
+
+Import  org.tio.utils.SystemTimer ;
+
+Import  com.xiaoleilu.hutool.date.BetweenFormater ;
+Import  com.xiaoleilu.hutool.date.BetweenFormater.Level ;
+
+/**
+ * This is for the server, mainly used to monitor the IP situation, black and malicious attack IP at any time
+ * @author tanyaowu
+ *  
+ */
