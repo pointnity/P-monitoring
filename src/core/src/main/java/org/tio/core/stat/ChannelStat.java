7f15c40beb987f8d2eb45dbfca7780f09faf7864
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-29 16:33:36 +0800

    Update ChannelStat.java

--- src/core/src/main/java/org/tio/core/stat/ChannelStat.java
+++ src/core/src/main/java/org/tio/core/stat/ChannelStat.java
@@ -15,3 +15,10 @@ public class ChannelStat implements java.io.Serializable {
 	 * Number of times this decoding failed
 	 */
 	private int decodeFailCount = 0;
+
+	/**
+	 * Last time a business message packet was received (a complete business message package, part of the message is not counted)
+	 */
+	private long latestTimeOfReceivedPacket = SystemTimer.currentTimeMillis();
+
+	/**
