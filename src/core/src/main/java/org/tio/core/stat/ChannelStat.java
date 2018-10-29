64f267d7afc4d8b10da61868e290e10ec3b573b4
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-29 16:33:13 +0800

    Update ChannelStat.java

--- src/core/src/main/java/org/tio/core/stat/ChannelStat.java
+++ src/core/src/main/java/org/tio/core/stat/ChannelStat.java
@@ -8,3 +8,10 @@ import org.tio.utils.SystemTimer;
  * @author tanyaowu
  *  
  */
+public class ChannelStat implements java.io.Serializable {
+	private static final long serialVersionUID = -6942731710053482089L;
+
+	/**
+	 * Number of times this decoding failed
+	 */
+	private int decodeFailCount = 0;
