775b3a1628a497977b5e9439a3290d15ac353815
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-30 12:11:08 +0800

    Create   UdpClient.java

---
+++ src/core/src/main/java/org/tio/core/Udp/UdpClient.java
@@ -0,0 +1,17 @@
+Package  org . tio . core . udp ;
+
+Import  java.io.UnsupportedEncodingException ;
+Import  java.net.DatagramPacket ;
+Import  java.net.InetSocketAddress ;
+Import  java.util.concurrent.LinkedBlockingQueue ;
+
+Import  org.apache.commons.lang3.StringUtils ;
+Import  org.slf4j.Logger ;
+Import  org.slf4j.LoggerFactory ;
+Import  org.tio.core.Node ;
+Import  org.tio.core.udp.task.UdpSendRunnable ;
+
+/**
+ * @author tanyaowu
+ *  
+ */
