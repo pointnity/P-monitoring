87e4d6502e97d9dfa4b5c9f98eb0a8dbaa756fed
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-30 11:44:36 +0800

    Create   DecodeRunnable.java

---
+++ src/core/src/main/java/org/tio/core/task/DecodeRunnable.java
@@ -0,0 +1,22 @@
+package org.tio.core.task;
+
+import java.nio.ByteBuffer;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.tio.core.Aio;
+import org.tio.core.ChannelAction;
+import org.tio.core.ChannelContext;
+import org.tio.core.GroupContext;
+import org.tio.core.PacketHandlerMode;
+import org.tio.core.exception.AioDecodeException;
+import org.tio.core.intf.AioListener;
+import org.tio.core.intf.Packet;
+import org.tio.core.stat.ChannelStat;
+import org.tio.core.stat.IpStat;
+import org.tio.core.utils.ByteBufferUtils;
+import org.tio.utils.SystemTimer;
+
+/**
+ * Decoding
