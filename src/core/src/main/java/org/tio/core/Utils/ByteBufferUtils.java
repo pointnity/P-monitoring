03785657de439002599d89776ad6867a5c2a2213
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-31 16:22:11 +0800

    Update ByteBufferUtils.java

--- src/core/src/main/java/org/tio/core/Utils/ByteBufferUtils.java
+++ src/core/src/main/java/org/tio/core/Utils/ByteBufferUtils.java
@@ -22,3 +22,22 @@ Public  class  ByteBufferUtils  {
 	 * @param byteBuffer1
 	 * @param byteBuffer2
 	 * @return
+	 * @author: tanyaowu
+	 */
+	Public  static  ByteBuffer  composite ( ByteBuffer  byteBuffer1 ,  ByteBuffer  byteBuffer2 )  {
+		Int  capacity  =  byteBuffer1 . limit ()  -  byteBuffer1 . position ()  +  byteBuffer2 . limit ()  -  byteBuffer2 . position ();
+		ByteBuffer  ret  =  ByteBuffer . allocate ( capacity );
+
+		Ret . put ( byteBuffer1 );
+		Ret . put ( byteBuffer2 );
+
+		Ret . position ( 0 );
+		Ret . limit ( ret . capacity ());
+		Return  ret ;
+	}
+
+	Public  static  void  copy ( ByteBuffer  src ,  int  srcStartindex ,  ByteBuffer  dest ,  int  destStartIndex ,  int  length )  {
+		System . arraycopy ( src . array (),  srcStartindex ,  dest . array (),  destStartIndex ,  length );
+	}
+
+	/**
