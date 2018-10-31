af27a9f1b2491614da14f525e0b886d498840dc8
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-31 15:55:36 +0800

    Update UdpConf.java

--- src/core/src/main/java/org/tio/core/udp/UdpConf.java
+++ src/core/src/main/java/org/tio/core/udp/UdpConf.java
@@ -16,3 +16,21 @@ Public  class  UdpConf  {
 	Public  static  void  main ( String []  args )  {
 
 	}
+
+	Private  int  timeout  =  5000 ;
+
+	Private  Node  serverNode  =  null ;
+
+	Private  String  charset  =  "utf-8" ;
+
+	/**
+	 *
+	 * @author tanyaowu
+	 */
+	Public  UdpConf ( int  timeout )  {
+		This . setTimeout ( timeout );
+	}
+
+	Public  String  getCharset ()  {
+		Return  charset ;
+	}
