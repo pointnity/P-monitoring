37b92b5a2ce8bbb790e175ae22f16e3901746948
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-31 16:18:44 +0800

    Update AioUtils.java

--- src/core/src/main/java/org/tio/core/Utils/AioUtils.java
+++ src/core/src/main/java/org/tio/core/Utils/AioUtils.java
@@ -11,3 +11,14 @@ import org.tio.utils.thread.ThreadUtils;
 /**
  * 
  * @author tanyaowu 
+ * 
+ */
+public class AioUtils {
+	private static Logger log = LoggerFactory.getLogger(AioUtils.class);
+
+	public static boolean checkBeforeIO(ChannelContext channelContext) {
+		boolean isClosed = channelContext.isClosed();
+		boolean isRemoved = channelContext.isRemoved();
+		boolean isWaitingClose = channelContext.isWaitingClose();
+
+		if (isWaitingClose) {
