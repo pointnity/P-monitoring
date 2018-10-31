1a5ae6996b07a690709ca94da3f131fcacf97be0
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-31 16:13:20 +0800

    Update UdpSendRunnable.java

--- src/core/src/main/java/org/tio/core/udp/Task/UdpSendRunnable.java
+++ src/core/src/main/java/org/tio/core/udp/Task/UdpSendRunnable.java
@@ -7,3 +7,8 @@ Import  java.util.concurrent.LinkedBlockingQueue ;
 Import  org.slf4j.Logger ;
 Import  org.slf4j.LoggerFactory ;
 Import  org.tio.core.udp.UdpConf ;
+
+/**
+ * @author tanyaowu
+ *  
+ */
