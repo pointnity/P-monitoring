3a2bf0ebca2b7ec4387994c7237bec6018c9d13b
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-31 16:27:00 +0800

    Update DebugUtils.java

--- src/core/src/main/java/org/tio/core/Utils/DebugUtils.java
+++ src/core/src/main/java/org/tio/core/Utils/DebugUtils.java
@@ -9,3 +9,15 @@ Import org.slf4j.LoggerFactory;
  */
 Public class DebugUtils
 {
+	Private static Logger log = LoggerFactory.getLogger(DebugUtils.class);
+
+	/**
+	 * 
+	 * @author tanyaowu
+	 */
+	Public DebugUtils()
+	{
+	}
+
+	Public static void printCost(CostIntf cost, String msg)
+	{
