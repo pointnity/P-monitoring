b4ca17baaaf34bd86d700abbc4415562e999adc3
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-31 15:55:25 +0800

    Update UdpConf.java

--- src/core/src/main/java/org/tio/core/udp/UdpConf.java
+++ src/core/src/main/java/org/tio/core/udp/UdpConf.java
@@ -6,3 +6,13 @@ Import  org.tio.core.Node ;
  * @author tanyaowu
  * 
  */
+Public  class  UdpConf  {
+	// private static Logger log = LoggerFactory.getLogger(UdpConf.class);
+
+	/**
+	 * @param args
+	 * @author tanyaowu
+	 */
+	Public  static  void  main ( String []  args )  {
+
+	}
