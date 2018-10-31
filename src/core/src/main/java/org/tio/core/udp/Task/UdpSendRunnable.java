400003715a83a7ca6b7f6035c0cfd80128c79e09
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-31 16:13:07 +0800

    Create UdpSendRunnable.java

---
+++ src/core/src/main/java/org/tio/core/udp/Task/UdpSendRunnable.java
@@ -0,0 +1,9 @@
+Package  org . tio . core . udp . task ;
+
+Import  java.net.DatagramPacket ;
+Import  java.net.DatagramSocket ;
+Import  java.util.concurrent.LinkedBlockingQueue ;
+
+Import  org.slf4j.Logger ;
+Import  org.slf4j.LoggerFactory ;
+Import  org.tio.core.udp.UdpConf ;
