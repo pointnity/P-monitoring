de35ea78125afca7d642e7a833683b82e1adc6ab
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-31 16:17:58 +0800

    Create   AioUtils.java

---
+++ src/core/src/main/java/org/tio/core/Utils/AioUtils.java
@@ -0,0 +1,13 @@
+package org.tio.core.utils;
+
+import java.nio.channels.AsynchronousSocketChannel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.tio.core.Aio;
+import org.tio.core.ChannelContext;
+import org.tio.utils.thread.ThreadUtils;
+
+/**
+ * 
+ * @author tanyaowu 
