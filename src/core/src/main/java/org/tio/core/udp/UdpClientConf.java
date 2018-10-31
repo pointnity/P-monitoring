1a0ef0c8b694bf204b4ab4092d2d2509fa651ecf
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-31 15:53:08 +0800

    Update UdpClientConf.java

--- src/core/src/main/java/org/tio/core/udp/UdpClientConf.java
+++ src/core/src/main/java/org/tio/core/udp/UdpClientConf.java
@@ -23,3 +23,5 @@ Public  class  UdpClientConf  extends  UdpConf  {
 	 */
 	Public  UdpClientConf ( String  serverip ,  int  serverport ,  int  timeout )  {
 		Super ( timeout );
+		Node  node  =  new  Node ( serverip ,  serverport );
+		the this . setServerNode ( Node );
