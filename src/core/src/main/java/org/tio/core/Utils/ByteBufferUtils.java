3ac6a858af6e1e28d9acbf7904d42d459dcdd4ff
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-31 16:21:38 +0800

    Update ByteBufferUtils.java

--- src/core/src/main/java/org/tio/core/Utils/ByteBufferUtils.java
+++ src/core/src/main/java/org/tio/core/Utils/ByteBufferUtils.java
@@ -6,3 +6,10 @@ Import  java.nio.ByteBuffer ;
 Import  org.apache.commons.lang3.StringUtils ;
 Import  org.slf4j.Logger ;
 Import  org.slf4j.LoggerFactory ;
+Import  org.tio.core.exception.LengthOverflowException ;
+
+/**
+ * 
+ * @author tanyaowu 
+ *  
+ */
