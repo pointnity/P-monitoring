4c5ed3c8782a8b5febb443e6ceedcae4b14b6aad
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-10-31 16:11:32 +0800

    Create   UdpHandlerRunnable.java

---
+++ src/core/src/main/java/org/tio/core/udp/Task/UdpHandlerRunnable.java
@@ -0,0 +1,11 @@
+Package  org . tio . core . udp . task ;
+
+Import  java.net.DatagramSocket ;
+Import  java.util.concurrent.LinkedBlockingQueue ;
+
+Import  org.slf4j.Logger ;
+Import  org.slf4j.LoggerFactory ;
+Import  org.tio.core.udp.UdpPacket ;
+Import  org.tio.core.udp.intf.UdpHandler ;
+
+/**
