8eb1611ad5210aae48b0dfb36064a4ac764556c3
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-11-01 16:47:57 +0800

    Update AcceptCompletionHandler.java

--- src/core/src/main/java/org/tio/Server/AcceptCompletionHandler.java
+++ src/core/src/main/java/org/tio/Server/AcceptCompletionHandler.java
@@ -47,3 +47,37 @@ Public  class  AcceptCompletionHandler  implements  CompletionHandler < Asynchro
 // IpStat ipStat = (IpStat) guavaCache.get(clientIp);
 // ipStat.getRequestCount().incrementAndGet();
 // }
+			
+			List < Long >  list  =  serverGroupContext . ipStats . durationList ;
+			For  ( Long  v  :  list )  {
+				IpStat  ipStat  =  ( IpStat )  serverGroupContext . ipStats . get ( v ,  clientIp );
+				ipStat . getRequestCount (). incrementAndGet ();
+			}
+
+			ServerGroupStat  serverGroupStat  =  serverGroupContext . getServerGroupStat ();
+			serverGroupStat . getAccepted (). incrementAndGet ();
+			
+			
+// channelContext.getIpStat().getActivatedCount().incrementAndGet();
+// for (GuavaCache guavaCache : caches) {
+// IpStat ipStat = (IpStat) guavaCache.get(clientIp);
+// ipStat.getActivatedCount().incrementAndGet();
+// }
+// for (Long v : list) {
+// IpStat ipStat = (IpStat) serverGroupContext.ips.get(v, clientIp);
+// IpStat.getActivatedCount().incrementAndGet();
+// }
+// IpStat.getActivatedCount(clientIp, true).incrementAndGet();
+
+			asynchronousSocketChannel . setOption ( StandardSocketOptions . SO_REUSEADDR ,  true );
+			asynchronousSocketChannel . setOption ( StandardSocketOptions . SO_RCVBUF ,  32  *  1024 );
+			asynchronousSocketChannel . setOption ( StandardSocketOptions . SO_SNDBUF ,  32  *  1024 );
+			asynchronousSocketChannel . setOption ( StandardSocketOptions . SO_KEEPALIVE ,  true );
+
+			ServerChannelContext  channelContext  =  new  ServerChannelContext ( serverGroupContext ,  asynchronousSocketChannel );
+			channelContext . setClosed ( false );
+			channelContext . setServerNode ( aioServer . getServerNode ());
+			ServerAioListener  serverAioListener  =  serverGroupContext . getServerAioListener ();
+			channelContext . getStat (). setTimeFirstConnected ( SystemTimer . currentTimeMillis ());
+			
+			channelContext . traceClient ( ChannelAction . CONNECT ,  null ,  null );
