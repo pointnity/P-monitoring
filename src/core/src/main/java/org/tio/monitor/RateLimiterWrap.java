595438594bec26708c8ebefdf9a3f21d4daa152d
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-11-01 16:40:22 +0800

    Create   RateLimiterWrap.java

---
+++ src/core/src/main/java/org/tio/monitor/RateLimiterWrap.java
@@ -0,0 +1,12 @@
+package org.tio.monitor;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.tio.utils.SystemTimer;
+
+import com.google.common.util.concurrent.RateLimiter;
+
+/**
+ * @author tanyaowu
+ *  
+ */
