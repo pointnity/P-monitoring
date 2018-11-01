76e7f9389c28695733d19828a9a8492fa9e8446f
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-11-01 16:47:42 +0800

    Update AcceptCompletionHandler.java

--- src/core/src/main/java/org/tio/Server/AcceptCompletionHandler.java
+++ src/core/src/main/java/org/tio/Server/AcceptCompletionHandler.java
@@ -20,3 +20,30 @@ Import  org.tio.utils.SystemTimer ;
  *
  * @author tanyaowu
  * 
+ */
+Public  class  AcceptCompletionHandler  implements  CompletionHandler < AsynchronousSocketChannel ,  AioServer >  {
+
+	Private  static  Logger  log  =  LoggerFactory . getLogger ( AcceptCompletionHandler . class );
+
+	Public  AcceptCompletionHandler ()  {
+	}
+
+	/**
+	 *
+	 * @param asynchronousSocketChannel
+	 * @param aioServer
+	 * @author tanyaowu
+	 */
+	@Override
+	Public  void  completed ( AsynchronousSocketChannel  asynchronousSocketChannel ,  AioServer  aioServer )  {
+		Try  {
+			ServerGroupContext  serverGroupContext  =  aioServer . getServerGroupContext ();
+			InetSocketAddress  inetSocketAddress  =  ( InetSocketAddress )  asynchronousSocketChannel . getRemoteAddress ();
+			String  clientIp  =  inetSocketAddress . getHostString ();
+// serverGroupContext.ips.get(clientIp).getRequestCount().incrementAndGet();
+			
+// GuavaCache[] caches = serverGroupContext.ips.getCaches();
+// for (GuavaCache guavaCache : caches) {
+// IpStat ipStat = (IpStat) guavaCache.get(clientIp);
+// ipStat.getRequestCount().incrementAndGet();
+// }
