4f029df0dfb7846b913e491457391aaf86c5e93c
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-11-02 16:47:36 +0800

    Create  AioServer.java

---
+++ src/core/src/main/java/org/tio/Server/AioServer.java
@@ -0,0 +1,17 @@
+package org.tio.server;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.StandardSocketOptions;
+import java.nio.channels.AsynchronousChannelGroup;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.tio.core.Node;
+
+/**
+ * @author tanyaowu
