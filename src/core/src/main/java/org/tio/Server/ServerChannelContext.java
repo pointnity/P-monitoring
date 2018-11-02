09b3a7210783adbb734353abbc61ad31e6f87288
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-11-02 16:51:16 +0800

    Update ServerChannelContext.java

--- src/core/src/main/java/org/tio/Server/ServerChannelContext.java
+++ src/core/src/main/java/org/tio/Server/ServerChannelContext.java
@@ -37,3 +37,8 @@ public class ServerChannelContext extends ChannelContext {
 	 *  
 	 *
 	 */
+	@Override
+	public Node createClientNode(AsynchronousSocketChannel asynchronousSocketChannel) throws IOException {
+		InetSocketAddress inetSocketAddress = (InetSocketAddress) asynchronousSocketChannel.getRemoteAddress();
+		Node clientNode = new Node(inetSocketAddress.getHostString(), inetSocketAddress.getPort());
+		return clientNode;
