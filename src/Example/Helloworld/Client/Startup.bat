eca424b20015f3ff58a1b25dbefeb98563da9999
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-11-05 18:10:43 +0800

    Update Startup.bat

--- src/Example/Helloworld/Client/Startup.bat
+++ src/Example/Helloworld/Client/Startup.bat
@@ -5,3 +5,5 @@ Setlocal & pushd
 Set APP_ENTRY=org.tio.examples.helloworld.client.HelloClientStarter
 Set BASE=%~dp0
 Set CP=%BASE%\config;%BASE%\lib\*
+Java -Xverify:none -XX:+HeapDumpOnOutOfMemoryError -Dtio.default.read.buffer.size=512 -XX:HeapDumpPath=c:/java-t-io-helloworld-client-pid.hprof -cp "%CP%" %APP_ENTRY%
+Endlocal & popd
