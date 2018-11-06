407427e569d40ffd6975c47d9a8c98f2438c4444
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-11-06 17:36:06 +0800

    Update HelloClientAioHandler.java

--- src/Example/Helloworld/Client/src/main/java/org/tio/examples/helloworld/client/HelloClientAioHandler.java
+++ src/Example/Helloworld/Client/src/main/java/org/tio/examples/helloworld/client/HelloClientAioHandler.java
@@ -6,3 +6,17 @@ Import  org.tio.client.intf.ClientAioHandler ;
 Import  org.tio.core.ChannelContext ;
 Import  org.tio.core.GroupContext ;
 Import  org.tio.core.exception.AioDecodeException ;
+Import  org.tio.core.intf.Packet ;
+Import  org.tio.examples.helloworld.common.HelloPacket ;
+
+/**
+ * 
+ * @author tanyaowu
+ */
+Public  class  HelloClientAioHandler  implements  ClientAioHandler  {
+	Private  static  HelloPacket  heartbeatPacket  =  new  HelloPacket ();
+
+
+	/**
+	 * Decoding: Decode the received ByteBuffer into a service message packet that the application can recognize.
+	 * Total message structure: message header + message body
