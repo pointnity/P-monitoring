09b732729b6dc97b77c1d49f63d9909033ec5553
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-11-06 17:35:55 +0800

    Create  HelloClientAioHandler.java

---
+++ src/Example/Helloworld/Client/src/main/java/org/tio/examples/helloworld/client/HelloClientAioHandler.java
@@ -0,0 +1,8 @@
+Package  org . tio . examples . helloworld . client ;
+
+Import  java.nio.ByteBuffer ;
+
+Import  org.tio.client.intf.ClientAioHandler ;
+Import  org.tio.core.ChannelContext ;
+Import  org.tio.core.GroupContext ;
+Import  org.tio.core.exception.AioDecodeException ;
