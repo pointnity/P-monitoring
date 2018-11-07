763b618289fa8176f4c1eca915c609be7938e79b
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-11-07 17:47:15 +0800

    Update Const.java

--- src/Example/Helloworld/Common/Src/main/java/org/tio/examples/helloworld/Common/Const.java
+++ src/Example/Helloworld/Common/Src/main/java/org/tio/examples/helloworld/Common/Const.java
@@ -5,3 +5,10 @@ Package  org . tio . examples . helloworld . common ;
  * @author tanyaowu
  *  
  */
+Public  interface  Const  {
+	/**
+	 * server address
+	 */
+	Public  static  final  String  SERVER  =  "127.0.0.1" ;
+	
+	/**
