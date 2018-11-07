563be8218031cdeeda6e6e2386847aeb62ad8c40
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-11-07 17:48:25 +0800

    Update HelloPacket.java

--- src/Example/Helloworld/Common/Src/main/java/org/tio/examples/helloworld/Common/HelloPacket.java
+++ src/Example/Helloworld/Common/Src/main/java/org/tio/examples/helloworld/Common/HelloPacket.java
@@ -5,3 +5,12 @@ Import  org.tio.core.intf.Packet ;
 /**
  * @author tanyaowu
  */
+Public  class  HelloPacket  extends  Packet  {
+	Private  static  final  long  serialVersionUID  =  - 172060606924066412L ;
+	Public  static  final  int  HEADER_LENGHT  =  4 ; // length of the message header
+	Public  static  final  String  CHARSET  =  "utf-8" ;
+	Private  byte []  body ;
+
+	/**
+	 * @return the body
+	 */
