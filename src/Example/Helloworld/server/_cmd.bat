a56d03f2a41b52950c374b28eff8bcd1c8facf35
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-11-09 19:26:00 +0800

    Update _cmd.bat

--- src/Example/Helloworld/server/_cmd.bat
+++ src/Example/Helloworld/server/_cmd.bat
@@ -3,3 +3,8 @@ Echo -
 Echo #download source code
 Echo mvn dependency:sources
 Echo -
+
+Echo #download source code jar. -DdownloadJavadocs=true Download javadoc package
+Echo -DdownloadSources=true
+Echo -
+Echo -
