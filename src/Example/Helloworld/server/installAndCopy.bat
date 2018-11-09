4bc8b50de326483bc07009afc4293e99e338b9ad
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-11-09 19:29:02 +0800

    Update installAndCopy.bat

--- src/Example/Helloworld/server/installAndCopy.bat
+++ src/Example/Helloworld/server/installAndCopy.bat
@@ -1,1 +1,4 @@
 Call mvn clean install
+
+Call rd ..\..\..\..\dist\examples\helloworld\server /s /q
+Call xcopy target\dist\tio-examples-helloworld-server-2.0.0.v20170824-RELEASE ..\..\..\..\dist\examples\helloworld\server\ /s /e /q /y
