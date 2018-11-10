ad45ad947f98d84d8623fe7cddb337fc510b7b7d
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-11-10 09:55:32 +0800

    Update Startup.bat

--- src/Example/Helloworld/server/Startup.bat
+++ src/Example/Helloworld/server/Startup.bat
@@ -4,3 +4,6 @@ Rem -Xms64m -Xmx2048m
 Setlocal & pushd
 Set APP_ENTRY=org.tio.examples.helloworld.server.HelloServerStarter
 Set BASE=%~dp0
+Set CP=%BASE%\config;%BASE%\lib\*
+Java -Xverify:none -XX:+HeapDumpOnOutOfMemoryError -Dtio.default.read.buffer.size=512 -XX:HeapDumpPath=c:/java-t-io-helloworld-server-pid.hprof -cp "%CP%" %APP_ENTRY%
+Endlocal & popd
