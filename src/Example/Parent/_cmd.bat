71936bb3815afb260346f7978c5e09e5986a33b8
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-11-11 12:46:40 +0800

    Update _cmd.bat

--- src/Example/Parent/_cmd.bat
+++ src/Example/Parent/_cmd.bat
@@ -3,3 +3,7 @@ Echo -
 Echo #download source code
 Echo mvn dependency:sources
 Echo -
+
+Echo #download source code jar. -DdownloadJavadocs=true Download javadoc package
+Echo -DdownloadSources=true
+Echo -
