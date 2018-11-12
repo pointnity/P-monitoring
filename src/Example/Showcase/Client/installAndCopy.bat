f30a7aa5138e1a60388c6f6c11d21e03d2bd7b13
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-11-12 16:30:00 +0800

    Update installAndCopy.bat

--- src/Example/Showcase/Client/installAndCopy.bat
+++ src/Example/Showcase/Client/installAndCopy.bat
@@ -1,1 +1,4 @@
 Call mvn clean install
+
+Call rd ..\..\..\..\dist\examples\showcase\client /s /q
+Call xcopy target\dist\tio-examples-showcase-client-2.0.0.v20170824-RELEASE ..\..\..\..\dist\examples\showcase\client\ /s /e /q /y
