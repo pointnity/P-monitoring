ee468793b4549d7a6a9559dab1630e83355441b8
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-11-16 18:41:31 +0800

    Update GroupMsgRespHandler.java

--- src/Example/Showcase/Client/Src/Main/Java/org/tio/examples/showcase/client/Handler/GroupMsgRespHandler.java
+++ src/Example/Showcase/Client/Src/Main/Java/org/tio/examples/showcase/client/Handler/GroupMsgRespHandler.java
@@ -5,3 +5,9 @@ Import  org.slf4j.LoggerFactory ;
 Import  org.tio.core.ChannelContext ;
 Import  org.tio.examples.showcase.common.ShowcasePacket ;
 Import  org.tio.examples.showcase.common.intf.AbsShowcaseBsHandler ;
+Import  org.tio.examples.showcase.common.packets.GroupMsgRespBody ;
+Import  org.tio.utils.json.Json ;
+
+/**
+ * @author tanyaowu
+ *  
