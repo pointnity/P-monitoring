d10bbe7bea2684a987caa9b97989a62d46d65270
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-11-16 18:47:55 +0800

    Update LoginRespHandler.java

--- src/Example/Showcase/Client/Src/Main/Java/org/tio/examples/showcase/client/Handler/LoginRespHandler.java
+++ src/Example/Showcase/Client/Src/Main/Java/org/tio/examples/showcase/client/Handler/LoginRespHandler.java
@@ -3,3 +3,13 @@ Package  org . tio . examples . showcase . client . handler .
 Import  org.slf4j.Logger ;
 Import  org.slf4j.LoggerFactory ;
 Import  org.tio.core.ChannelContext ;
+Import  org.tio.examples.showcase.common.ShowcasePacket ;
+Import  org.tio.examples.showcase.common.ShowcaseSessionContext ;
+Import  org.tio.examples.showcase.common.intf.AbsShowcaseBsHandler ;
+Import  org.tio.examples.showcase.common.packets.LoginRespBody ;
+Import  org.tio.utils.json.Json ;
+
+/**
+ * @author tanyaowu
+ *  
+ */
