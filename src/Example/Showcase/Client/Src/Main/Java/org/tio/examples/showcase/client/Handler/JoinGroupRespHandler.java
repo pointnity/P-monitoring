aa076b3ffff65260aed4bc31bbe6322fc4798851
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-11-16 18:45:03 +0800

    Update JoinGroupRespHandler.java

--- src/Example/Showcase/Client/Src/Main/Java/org/tio/examples/showcase/client/Handler/JoinGroupRespHandler.java
+++ src/Example/Showcase/Client/Src/Main/Java/org/tio/examples/showcase/client/Handler/JoinGroupRespHandler.java
@@ -5,3 +5,9 @@ Import  org.slf4j.LoggerFactory ;
 Import  org.tio.core.Aio ;
 Import  org.tio.core.ChannelContext ;
 Import  org.tio.examples.showcase.common.ShowcasePacket ;
+Import  org.tio.examples.showcase.common.intf.AbsShowcaseBsHandler ;
+Import  org.tio.examples.showcase.common.packets.JoinGroupRespBody ;
+Import  org.tio.utils.json.Json ;
+
+/**
+ * @author tanyaowu
