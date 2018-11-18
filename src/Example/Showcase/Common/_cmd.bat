e99012b233f1a0a3713a896a7d4c0f8161755fd3
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-11-18 22:23:59 +0800

    Update _cmd.bat

--- src/Example/Showcase/Common/_cmd.bat
+++ src/Example/Showcase/Common/_cmd.bat
@@ -12,3 +12,9 @@ Echo -
 
 
 Echo # jar out
+Echo mvn dependency:unpack-dependencies
+Echo -
+
+Echo #Copy the jar to a directory (all jars are in the same directory)
+Echo mvn dependency:copy-dependencies -Dmdep.useRepositoryLayout=false
+Echo -
