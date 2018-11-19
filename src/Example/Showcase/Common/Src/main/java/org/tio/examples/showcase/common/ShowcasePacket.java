5fc42d9848746337728e1be4509678a898c25885
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-11-19 10:59:54 +0800

    Update ShowcasePacket.java

--- src/Example/Showcase/Common/Src/main/java/org/tio/examples/showcase/common/ShowcasePacket.java
+++ src/Example/Showcase/Common/Src/main/java/org/tio/examples/showcase/common/ShowcasePacket.java
@@ -7,3 +7,5 @@ Import  org.tio.core.intf.Packet ;
  * @author tanyaowu
  */
 Public  class  ShowcasePacket  extends  Packet  {
+	Private  static  final  long  serialVersionUID  =  - 5481926483435771100L ;
+	Public  static  final  int  HEADER_LENGHT  =  5 ; //The length of the message header is 1+4
