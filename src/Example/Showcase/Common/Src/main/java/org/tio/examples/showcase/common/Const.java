4b1a2a5d23b3794fcbfc0167f1c7340cc7e425a3
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-11-19 10:54:56 +0800

    Update Const.java

--- src/Example/Showcase/Common/Src/main/java/org/tio/examples/showcase/common/Const.java
+++ src/Example/Showcase/Common/Src/main/java/org/tio/examples/showcase/common/Const.java
@@ -8,3 +8,7 @@ Package  org . tio . examples . showcase . common ;
 Public  interface  Const  {
 
 	Public  static  final  int  PORT  =  5678 ;
+
+	Public  static  final  String  CHARSET  =  "utf-8" ;
+
+}
