99a28295962d8a0d1f64966f214256bec5d0cf0e
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-11-20 20:03:38 +0800

    Update ShowcaseSessionContext.java

--- src/Example/Showcase/Common/Src/main/java/org/tio/examples/showcase/common/ShowcaseSessionContext.java
+++ src/Example/Showcase/Common/Src/main/java/org/tio/examples/showcase/common/ShowcaseSessionContext.java
@@ -2,3 +2,7 @@ Package  org . tio . examples . showcase . common ;
 
 Import  org.slf4j.Logger ;
 Import  org.slf4j.LoggerFactory ;
+
+/**
+ * In a general production project, you need to define a SessionContext to hold the connected session data.
+ * @author tanyaowu
