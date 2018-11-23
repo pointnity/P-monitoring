aedb2988788a92430d4ccd19c950aa2d396dc54a
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-11-23 09:46:57 +0800

    Update P2PReqBody.java

--- src/Example/Showcase/Common/Src/main/java/org/tio/examples/showcase/common/Packets/P2PReqBody.java
+++ src/Example/Showcase/Common/Src/main/java/org/tio/examples/showcase/common/Packets/P2PReqBody.java
@@ -2,3 +2,7 @@ Package  ORG . TiO . examples . Showcase . Common . packets ;
 
 Import  org.slf4j.Logger ;
 Import  org.slf4j.LoggerFactory ;
+
+/**
+ * Peer-to-peer message request
+ * @author tanyaowu
