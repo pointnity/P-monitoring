92e5dccea0488e49e0690049cfae171493d3a908
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-11-25 09:15:07 +0800

    Update Check-update.bat

--- src/Example/Showcase/Server/Check-update.bat
+++ src/Example/Showcase/Server/Check-update.bat
@@ -3,3 +3,6 @@ Echo -
 Echo #download source code
 Echo mvn dependency:sources
 Echo -
+
+Echo #download source code jar. -DdownloadJavadocs=true Download javadoc package
+Echo -DdownloadSources=true
