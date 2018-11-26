234e05e4d8bc9a40760cc7dab4588d07f3faf2bb
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-11-26 15:29:14 +0800

    Update Startup.bat

--- src/Example/Showcase/Server/Startup.bat
+++ src/Example/Showcase/Server/Startup.bat
@@ -5,3 +5,5 @@ Setlocal & pushd
 Set APP_ENTRY=org.tio.examples.showcase.server.ShowcaseServerStarter
 Set BASE=%~dp0
 Set CP=%BASE%\config;%BASE%\lib\*
+Java -Xverify:none -XX:+HeapDumpOnOutOfMemoryError -Dtio.default.read.buffer.size=512 -XX:HeapDumpPath=c:/java-t-io-showcase-server-pid.hprof -cp "%CP%" %APP_ENTRY%
+Endlocal & popd
