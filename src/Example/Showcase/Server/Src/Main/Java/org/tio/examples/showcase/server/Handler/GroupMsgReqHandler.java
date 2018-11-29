9a99dd12ab20edbb21c91040a3a4b5db47a7e467
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-11-29 16:30:32 +0800

    Update GroupMsgReqHandler.java

--- src/Example/Showcase/Server/Src/Main/Java/org/tio/examples/showcase/server/Handler/GroupMsgReqHandler.java
+++ src/Example/Showcase/Server/Src/Main/Java/org/tio/examples/showcase/server/Handler/GroupMsgReqHandler.java
@@ -7,3 +7,6 @@ Import  org.tio.core.ChannelContext ;
 Import  org.tio.examples.showcase.common.ShowcasePacket ;
 Import  org.tio.examples.showcase.common.Type ;
 Import  org.tio.examples.showcase.common.intf.AbsShowcaseBsHandler ;
+Import  org.tio.examples.showcase.common.packets.GroupMsgReqBody ;
+Import  org.tio.examples.showcase.common.packets.GroupMsgRespBody ;
+Import  org.tio.utils.json.Json ;
