54c40c2b55d866c0e859ee4ff1b4a05676cf6337
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-11-30 15:22:50 +0800

    Update HeartbeatReqHandler.java

--- src/Example/Showcase/Server/Src/Main/Java/org/tio/examples/showcase/server/Handler/HeartbeatReqHandler.java
+++ src/Example/Showcase/Server/Src/Main/Java/org/tio/examples/showcase/server/Handler/HeartbeatReqHandler.java
@@ -6,3 +6,6 @@ Import  org.tio.core.ChannelContext ;
 Import  org.tio.examples.showcase.common.ShowcasePacket ;
 Import  org.tio.examples.showcase.common.intf.AbsShowcaseBsHandler ;
 Import  org.tio.examples.showcase.common.packets.GroupMsgReqBody ;
+
+/**
+ * Heartbeat processing
