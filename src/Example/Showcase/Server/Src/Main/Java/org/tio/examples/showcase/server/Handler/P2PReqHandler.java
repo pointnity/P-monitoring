f5fad33b3df5eda6119a8b9d5a29864d87a35d75
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-12-01 14:48:30 +0800

    Update P2PReqHandler.java

--- src/Example/Showcase/Server/Src/Main/Java/org/tio/examples/showcase/server/Handler/P2PReqHandler.java
+++ src/Example/Showcase/Server/Src/Main/Java/org/tio/examples/showcase/server/Handler/P2PReqHandler.java
@@ -4,3 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.tio.core.Aio;
 import org.tio.core.ChannelContext;
+import org.tio.examples.showcase.common.ShowcasePacket;
+import org.tio.examples.showcase.common.ShowcaseSessionContext;
+import org.tio.examples.showcase.common.Type;
