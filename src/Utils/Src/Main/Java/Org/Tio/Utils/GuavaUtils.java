65a1465e105710b7a4ff526bf38cd2254890d5e4
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-12-07 11:43:06 +0800

    Update GuavaUtils.java

--- src/Utils/Src/Main/Java/Org/Tio/Utils/GuavaUtils.java
+++ src/Utils/Src/Main/Java/Org/Tio/Utils/GuavaUtils.java
@@ -25,3 +25,5 @@ Public  class  GuavaUtils  {
 	 * @param timeToLiveSeconds Set the expiration time after write cache (unit: second)
 	 * @param timeToIdleSeconds Sets the expiration time after reading the cache (unit: second)
 	 * @param initialCapacity
+	 * @param maximumSize
+	 * @param recordStats
