3dacaa27475d95d61b132c4af180a8a3ace36cc8
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-12-17 16:54:05 +0800

    Update SetWithLock.java

--- src/Utils/Src/Main/Java/Org/Tio/Utils/Lock/SetWithLock.java
+++ src/Utils/Src/Main/Java/Org/Tio/Utils/Lock/SetWithLock.java
@@ -12,3 +12,9 @@ Import  org.slf4j.LoggerFactory ;
  * @author tanyaowu
  * 
  */
+Public  class  SetWithLock < T >  extends  ObjWithLock < Set < T >>  {
+	Private  static  final  long  serialVersionUID  =  - 2305909960649321346L ;
+	Private  static  final  Logger  log  =  LoggerFactory . getLogger ( SetWithLock . class );
+
+	/**
+	 * @param set
