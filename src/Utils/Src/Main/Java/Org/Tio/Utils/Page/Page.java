30ce5dd3c7471caef4d024fcc705d68eec8fdc39
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-12-18 16:41:32 +0800

    Update Page.java

--- src/Utils/Src/Main/Java/Org/Tio/Utils/Page/Page.java
+++ src/Utils/Src/Main/Java/Org/Tio/Utils/Page/Page.java
@@ -12,3 +12,5 @@ Public  class  Page < T >  implements  Serializable  {
 
 	Private  static  final  long  serialVersionUID  =  6551482606063638959L ;
 	Private  List < T >  pageData  =  null ;  //data of the current page
+	Private  Integer  pageIndex ;  //The current page number, starting from 1, if the value is less than or equal to 0, it is regarded as 1
+	Private  Integer  pageSize ;  //Number of records per page
