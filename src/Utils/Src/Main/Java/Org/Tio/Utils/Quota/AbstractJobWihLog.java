40244200ede04f424b950899bf1715cb8877b549
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-12-21 17:07:36 +0800

    Update AbstractJobWihLog.java

--- src/Utils/Src/Main/Java/Org/Tio/Utils/Quota/AbstractJobWihLog.java
+++ src/Utils/Src/Main/Java/Org/Tio/Utils/Quota/AbstractJobWihLog.java
@@ -3,3 +3,5 @@ Package  org . tio . utils . quartz ;
 Import  org.quartz.Job ;
 Import  org.quartz.JobExecutionContext ;
 Import  org.quartz.JobExecutionException ;
+Import  org.slf4j.Logger ;
+Import  org.slf4j.LoggerFactory ;
