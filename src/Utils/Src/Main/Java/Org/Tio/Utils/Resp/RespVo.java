e93e652486a1451af668f682626a1248c1633a7d
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-12-23 15:33:21 +0800

    Update RespVo.java

--- src/Utils/Src/Main/Java/Org/Tio/Utils/Resp/RespVo.java
+++ src/Utils/Src/Main/Java/Org/Tio/Utils/Resp/RespVo.java
@@ -7,3 +7,6 @@ Package  org . tio . utils . resp ;
 Public  class  RespVo  implements  java . io . Serializable  {
 	Private  static  final  long  serialVersionUID  =  7492427869347211588L ;
 	// private static Logger log = LoggerFactory.getLogger(RespVo.class);
+
+	Public  static  RespVo  fail ()  {
+		RespVo  resp  =  new  RespVo ( RespResult . FAIL );
